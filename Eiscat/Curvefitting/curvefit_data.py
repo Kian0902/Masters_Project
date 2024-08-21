30918110aea88d475c22c3d56d610ce11ffc90dd
Kian Sartipzadeh <kian0902@hotmail.com>
2024-08-21 13:33:03 +0200

    Made Neural Network class for curvefitting purposes. Added attrubute function to class.

--- Eiscat/Curvefitting/curvefit_data.py
+++ Eiscat/Curvefitting/curvefit_data.py
@@ -8,12 +8,32 @@ Created on Wed Aug 21 10:31:39 2024
 
 
 
+import os
+import pickle
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+import torch
+import torch.nn as nn
 
 
 
 
 
-
+class Curvefit(nn.Module):
+    """
+    Class for curvefitting using a Neural Network. 
+    """
+    
+    def __init__(self):
+        super(Curvefit, self).__init__()
+        
+        # Setting up parameters to be optimized
+        self.HE_below = nn.Parameter(torch.tensor([np.log(5)], dtype=torch.float64), requires_grad=True)
+        self.HE_above = nn.Parameter(torch.tensor([np.log(10)], dtype=torch.float64), requires_grad=True)
+        self.HF_below = nn.Parameter(torch.tensor([np.log(25)], dtype=torch.float64), requires_grad=True)
+        self.HF_above = nn.Parameter(torch.tensor([np.log(40)], dtype=torch.float64), requires_grad=True)
 
 
 
