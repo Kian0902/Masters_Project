73ecd5a0af2c53b4dabffe39dceab309421b71a5
Kian Sartipzadeh <kian0902@hotmail.com>
2024-08-22 15:13:59 +0200

    Created new python file for filtering data processed by the 'EISCATDataSorter' from the 'temp_processing' file.

---
+++ Eiscat/Processing/temp_filter_data.py
@@ -0,0 +1,78 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Aug 22 15:10:52 2024
+
+@author: Kian Sartipzadeh
+"""
+
+import os
+import pickle
+
+import numpy as np
+from scipy.io import loadmat
+from scipy.interpolate import interp1d
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- Eiscat/Processing/temp_processing.py
+++ Eiscat/Processing/temp_processing.py
@@ -62,7 +62,7 @@ class EISCATDataSorter:
         data  (dict)  | Dictionary containing data per wanted key
         """
         
-        # if tesing is activated
+        # if testing is activated
         if testing==True:
             print("Keys before processing:")
             for key in loadmat(file):
@@ -80,9 +80,6 @@ class EISCATDataSorter:
             data['r_h'] = np.tile(data['r_h'], (1, data["r_param"].shape[0])).T  # copying r_h such that is has the same shape as r_param
         else:
             pass
-        
-        
-        
         
         return data
     
@@ -189,98 +186,7 @@ class EISCATDataSorter:
 folder_name = "Ne"
 A = EISCATDataSorter(folder_name)
 
-a = A.test_dataflow()
-
-
-
-# print(len(A.dataset.keys()))
-# print(A.dataset['2018-11-10']['r_param'].shape)
-
-# file_paths = A.get_file_paths()
-
-# A.sort_data()
-
-
-# class EISCATDataSorter:
-#     """
-#     Class for importing and sorting EISCAT data.
-#     """
-#     def __init__(self, full_path_to_folder: str):
-
-        
-#         self.full_path = full_path_to_folder
-#         self.dataset = {}  # for storing dataset
-        
-#     def get_file_paths(self):
-
-#         file_paths = [os.path.join(root, file) 
-#                       for root, dirs, files in os.walk(self.full_path) 
-#                       for file in files if file.endswith('.mat')]
-        
-        
-#         return file_paths
-
-#     def process_file(self, file: str):
-
-#         data = loadmat(file)  # importing matlab file as dictionary
-#         include = ["r_h", "r_param", "r_error"]  # keys to include
-#         data = {key: data[key] for key in data if key in include}
-        
-#         return data
-    
-    
-    
-#     def sort_data(self, save_data: bool=False):
-        
-#         files = self.get_file_paths()
-        
-#         for i, file in enumerate(files):
-            
-#             data = self.process_file(file)
-            
-                    
-                    
-#     def return_data(self):
-#             """Returns the sorted dataset."""
-#             return self.dataset
-
-
-
-
-
-
-# # Main path to Folder containing EISCAT data
-# path = "C:\\Users\\kian0\\OneDrive\\Desktop\\UiT Courses\\FYS_3931\\Scripts\\Masters_Project\\Eiscat\\Processing\\Ne"
-
-
-# folder = os.listdir(path)
-# A = EISCATDataSorter(path)
-
-# file_paths = A.get_file_paths()
-
-
-# A.sort_data()
-
-# file_paths = [os.path.join(root, file) 
-#               for root, dirs, files in os.walk(path) 
-#               for file in files if file.endswith('.mat')]
-
-# First file
-# f = file_paths[0]
-
-# Convert .mat to dict
-# data = A.process_file(f)
-
-# ne = data['r_param'].T
-# z  = data['r_h']
-
-# print(ne.shape)
-# print(z.shape)
-
-# plt.plot(ne[:,0:10], z)
-# plt.xscale("log")
-# plt.show()
-
+a = A.sort_data()
 
 
 
