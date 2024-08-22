76b64deef844e7232978fc2ef40853b3b7dbc337
Kian Sartipzadeh <kian0902@hotmail.com>
2024-08-22 12:34:41 +0200

    Starting making class for sorting EISCAT data.

--- Eiscat/Processing/temp_processing.py
+++ Eiscat/Processing/temp_processing.py
@@ -15,10 +15,36 @@ import matplotlib.pyplot as plt
 
 
 
+
+class EISCATDataSorter:
+    """
+    Class for importing and sorting EISCAT data.
+    """
+    def __init__(self, global_folder_path: str, global_folder_name: str):
+        """
+        Attributes (type)          | DESCRIPTION
+        ------------------------------------------------
+        global_folder_path (str)   | Global path of folder containing EISCAT data
+        global_folder_name (str)   | Name of folder containing EISCAT data
+        dataset            (dict)  | Dict for storing data
+        """
+        self.global_folder_path = global_folder_path
+        self.global_folder_name = global_folder_name
+        self.dataset = {}  # for storing dataset
+
+
+
+
+
+
+
+
+
+
+
+
 # Main path to Folder containing EISCAT data
 path = "C:\\Users\\kian0\\OneDrive\\Desktop\\UiT Courses\\FYS_3931\\Scripts\\Masters_Project\\Eiscat\\Processing\\Ne"
-
-
 
 
 folder = os.listdir(path)
@@ -27,22 +53,17 @@ file_paths = [os.path.join(root, file)
               for root, dirs, files in os.walk(path) 
               for file in files if file.endswith('.mat')]
 
-
-
 # First file
 f = file_paths[0]
 
 # Convert .mat to dict
 data = loadmat(f)
 
-
 ne = data['r_param'].T
 z  = data['r_h']
 
 print(ne.shape)
 print(z.shape)
-
-
 
 plt.plot(ne[:,0:10], z)
 plt.xscale("log")
