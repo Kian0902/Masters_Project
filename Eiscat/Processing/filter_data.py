a9ffbb7cdc0a412fcb04fe3a3cbd58e0c0c7205b
Kian Sartipzadeh <kian0902@hotmail.com>
2024-08-22 16:56:15 +0200

    Moved codes from temporary python files to the main files before deleting the temp files.

--- Eiscat/Processing/filter_data.py
+++ Eiscat/Processing/filter_data.py
@@ -1,13 +1,9 @@
 # -*- coding: utf-8 -*-
 """
-Created on Mon Aug 19 14:56:19 2024
+Created on Thu Aug 22 15:10:52 2024
 
 @author: Kian Sartipzadeh
 """
-
-
-
-
 
 import os
 import pickle
@@ -18,25 +14,26 @@ from scipy.interpolate import interp1d
 
 
 
-
 class DataFiltering:
     """
-    Class for clipping and filtering data with dictionaries. 
+    Class for clipping and filtering dict data.
     """
     def __init__(self, dataset):
         """
         Attributes (type)  | DESCRIPTION
         ------------------------------------------------
-        dataset (dict)     | Single dictionary containing the matlab data.
+        dataset (dict)     | Single dictionary containing processed data.
         """
         self.dataset = dataset
+    
     
     
     def filter_range(self, key: str, min_val: float, max_val: float):
         """
         Filters out values that lie outside an interval [min_val, max_mal]
-        specified by the user. This is assuming that each key has an ndarray
-        as value. In other words,  key:ndarray
+        specified by the user.
+        It is assumed that each key in self.dataset is assigned a (N x M)
+        numpy.ndarray.
         
         Input (type)    | DESCRIPTION
         ------------------------------------------------
@@ -45,23 +42,23 @@ class DataFiltering:
         max_val (float) | Upper limit of interval
         """
         
+        
         # Mask for filtered values. False outside and True inside interval
-        mask = (self.dataset[key] >= min_val) & (self.dataset[key] <= max_val)
-        mask = mask.ravel()
+        mask = np.any((self.dataset[key] >= min_val) & (self.dataset[key] <= max_val), axis=1)
         
         # Applying mask to all keys
-        for k in list(self.dataset.keys())[1:]:
-            self.dataset[k] = self.dataset[k][mask, 0]
+        for key in list(self.dataset.keys()):
+            self.dataset[key] = self.dataset[key][mask,:]
             
-            
-        
-
-    def handle_nan(self, replace_val):
+    
+    
+    def filter_nan(self, replace_val=111):
         """
-        Function for handling arrays with nan values. This method interpolates
-        and/or extrapolates way nans. If more than half of the array contains
-        nans, then user has the choise of which value to fill the entire array
-        with.
+        Function for handling numpy.ndarrays with NaN values. This method
+        interpolates and/or extrapolates way NaNs. If more than half of the
+        array contains NaNs, then user has the choice of picking a fill value
+        to replace the entire array. This is so it becomes easier later to 
+        distinguish faulty arrays from healthy ones.
         
         Input (type)            | DESCRIPTION
         ------------------------------------------------
@@ -69,35 +66,36 @@ class DataFiltering:
         """
         
         # Looping through keys
-        # print(len(self.dataset.keys()))
         for key in self.dataset:
+            data = self.dataset[key]
             
-            
-            
-            
-            # If any values within key is nan
-            if np.isnan(self.dataset[key]).any() == True:
-                nan_indices = np.isnan(self.dataset[key]) # Find the indices of NaN values
+            # Check if there are any NaNs in the data
+            if np.isnan(data).any():
+                # Find the indices of NaN values
+                nan_mask = np.isnan(data)
                 
-                
-                # If more than half of array contains nan
-                """ This is so that faulty data can be detected"""
-                if len(nan_indices[nan_indices == True]) > len(self.dataset[key])/2:
+                # If more than half of the elements in the array are NaN
+                if np.sum(nan_mask) > (data.shape[0] * data.shape[1]) / 2:
+                    # Fill the entire array with replace_val
                     self.dataset[key].fill(replace_val)
-
-
-                # If array contains any other instances of nans
                 else:
+                    # Interpolate/extrapolate the NaNs for each column (minute)
+                    x = np.arange(data.shape[0])  # Indexes for rows (altitude measurements)
                     
-                    self.dataset[key][nan_indices] = interp1d(np.arange(len(self.dataset[key]))[~nan_indices], 
-                                              self.dataset[key][~nan_indices], 
-                                              kind='linear', fill_value="extrapolate")(np.arange(len(self.dataset[key]))[nan_indices])
+                    for j in range(data.shape[1]):  # Iterate over each column (minute)
+                        col = data[:, j]
+                        valid_mask = ~nan_mask[:, j]
+                        if np.any(valid_mask):  # Check if there's at least one valid point
+                            interp_func = interp1d(x[valid_mask], col[valid_mask], 
+                                                   kind='linear', fill_value="extrapolate")
+                            col[~valid_mask] = interp_func(x[~valid_mask])
+                        else:
+                            # If the entire column is NaN, fill it with replace_val
+                            data[:, j].fill(replace_val)
                     
-                    
-            # If nan is not detected
-            else:
-                pass
-                
+                    self.dataset[key] = data
+
+
 
 
     def return_data(self):
@@ -105,11 +103,6 @@ class DataFiltering:
         Returns self.data
         """
         return self.dataset
-        
-
-
-
-
 
 
 
--- Eiscat/Processing/sorting_data.py
+++ Eiscat/Processing/sorting_data.py
@@ -1,29 +1,19 @@
 # -*- coding: utf-8 -*-
 """
-Created on Mon Aug 19 15:06:46 2024
+Created on Thu Aug 22 10:26:02 2024
 
 @author: Kian Sartipzadeh
 """
+
 
 
 import os
 import pickle
 import numpy as np
 from scipy.io import loadmat
-from filter_data import DataFiltering
+import matplotlib.pyplot as plt
 
-"""
-To do:
-    - Add more explanation to 'sort_data' method
-"""
-
-
-
-
-"""
-dataset is a nested dictionary.   key_global: key_local1, key_local2
-                                  key_local1: ndarray
-"""
+from temp_filter_data import DataFiltering
 
 
 
@@ -31,38 +21,38 @@ class EISCATDataSorter:
     """
     Class for importing and sorting EISCAT data.
     """
-    def __init__(self, global_folder_path: str, global_folder_name: str):
+    def __init__(self, folder_name: str):
         """
         Attributes (type)          | DESCRIPTION
         ------------------------------------------------
-        global_folder_path (str)   | Global path of folder containing EISCAT data
-        global_folder_name (str)   | Name of folder containing EISCAT data
-        dataset            (dict)  | Dict for storing data
+        folder_name (str)   | Name of folder containing .mat data files
+        dataset     (dict)  | Dict for storing data
         """
-        self.global_folder_path = global_folder_path
-        self.global_folder_name = global_folder_name
-        self.dataset = {}  # for storing dataset
-    
-    
-    def get_subfolder_names(self):
+        self.full_path = os.path.abspath(os.path.join(os.getcwd(), folder_name))  # Find full dir path
+        self.dataset = {}
+
+
+
+    def get_file_paths(self):
         """
-        Get subfolder names in the global folder. Each subfolder represents
-        a days worth of EISCAT measurements.
+        Getting the full dir path of each .mat data file.
         
         Return (type)            | DESCRIPTION
         ------------------------------------------------
-        subfolder_names (list)   | list containing subfolder names
+        file_paths (list[str])   | List containing full .mat path names
         """
-        subfolder_names = [os.path.join(self.global_folder_name, subdir) 
-                           for root, dirs, files in os.walk(self.global_folder_path) 
-                           for subdir in dirs]
-        return subfolder_names
+        
+        file_paths = [os.path.join(root, file) 
+                      for root, dirs, files in os.walk(self.full_path) 
+                      for file in files if file.endswith('.mat')]
+        
+        return file_paths
 
 
 
-    def process_file(self, file: str):
+    def process_file(self, file: str, testing: bool=False):
         """
-        Load and filter data from a single matlab file
+        Load data from a single .mat data file
         
         Input (type) | DESCRIPTION
         ------------------------------------------------
@@ -70,68 +60,158 @@ class EISCATDataSorter:
         
         Return (type) | DESCRIPTION
         ------------------------------------------------
-        data  (dict)  | Dictionary containing data from wanted keys
+        data  (dict)  | Dictionary containing data per wanted key
         """
-        data = loadmat(file)  # importing matlab file as dictionary
-        include = ["r_time", "r_h", "r_param", "r_error"]  # keys to include
-        data = {key: data[key] for key in data if key in include}
+        
+        # if testing is activated
+        if testing==True:
+            print("Keys before processing:")
+            for key in loadmat(file):
+                print(f" - {key}")
+            print("\n")
         
         
-        process = DataFiltering(data)
-        process.filter_range("r_h", 90, 400)
-        process.handle_nan(replace_val=666)
+        data = loadmat(file)  # importing .mat file as dict
+        include = ["r_h", "r_param", "r_error"]
+        data = {key: data[key].T for key in data if key in include}
+        
+        
+
+        
+        # if detecting different shapes
+        if data['r_h'].shape != data["r_param"].shape:
+            data['r_h'] = np.tile(data['r_h'], (data["r_param"].shape[1], 1)).T  # copying r_h such that is has the same shape as r_param
+        else:
+            pass
+        
+        
+        # Applying filers
+        filt = DataFiltering(data)
+        filt.filter_range('r_h', 90, 400)    
+        filt.filter_nan()
         return data
     
     
-
+    
     def sort_data(self, save_data: bool=False):
         """
-        Sort the data from the global folder.
+        Sort the data from folder containing .mat data files.
         """
+        files = self.get_file_paths()  # getting full path of .mat files
         
-        subfolder_names = self.get_subfolder_names()
+        for i, file in enumerate(files):
+            data = self.process_file(file)           # open and convert matlab file to dict
+            file_name = os.path.basename(file)[:-4]  # only get date from filename
+            self.dataset[file_name] = data           # assign data to date of measurement
+            
         
-        for i, subfolder in enumerate(subfolder_names):
-            
-            
-            # Condition for reducing number of print statements
-            if len(subfolder_names) > 100:
-                print(f'Processed: {i+1} of {len(subfolder_names)} days')
-            else:
-                print(f'Processed: {i+1} of {len(subfolder_names)} days')
-                
-                
-            # Getting matlab files from subfolder
-            files = [filename.path for filename in os.scandir(subfolder)
-                                if (filename.is_file()) and (filename.path[-3:] == 'mat')]
-            
-            # Getting data stat at 15min interval
-            files_to_process = [file for file in files if loadmat(file)["r_time"][0][4] % 15 == 0]
-            
-            day = subfolder[10:20]  # date of subfolder containing data
-            self.dataset[day] = {'r_time': [], 'r_h': [], 'r_param': [], 'r_error': []}
-            
-            # Processing each matlab file
-            for file in files_to_process:
-                data = self.process_file(file)
-                
-                # Assigning data to corresponding key
-                for key in self.dataset[day]:
-                    self.dataset[day][key].append(np.array(data[key]))
-                    
         if save_data == True:
             self.save_dataset()
-
+        
+        
+    
     def save_dataset(self, output_file='sorted_data.pkl'):
+        """
+        Saves dataset locally as a .pkl file.
+        """
         with open(output_file, 'wb') as file:
             pickle.dump(self.dataset, file)
 
-    
+
+
     def return_data(self):
-            """Returns the sorted dataset."""
-            return self.dataset
-    
-    
+        """
+        Returns the sorted dataset.
+        """
+        return self.dataset
+
+
+
+
+
+    def test_dataflow(self):
+        """
+        Tests the dataflow through the entire process using one file.
+        Prints the type and shape of the data at each step.
+        """
+        files = self.get_file_paths()
+
+        if not files:
+            return print("No .mat files found in the specified directory.")
+
+
+
+        # Use the first file for testing
+        test_file = files[0]
+        print(f"Testing with file: {os.path.basename(test_file)}")
+
+
+        # Test get_file_paths method
+        print("\nStep 1: get_file_paths()\n")
+        print(f"Output Type: {type(files)}")
+        print(f"Output Length: {len(files)}")
+        print(f"First File Path: {files[0] if files else 'None'}")
+        print("_____________________________________________")
+        
+        
+        # Test process_file method
+        print("\nStep 2: process_file()\n")
+        data = self.process_file(test_file, testing=True)
+        print(f"Input Type: {type(test_file)}")
+        print(f"Input: {test_file}")
+        print(f"Output Type: {type(data)}")
+        print(f"Output Keys: {list(data.keys())}")
+        for key in data:
+            print(f" - {key}: Shape = {data[key].shape}")
+        print("_____________________________________________")
+        
+        
+        # Test sort_data method with just this file
+        print("\nStep 3: sort_data()\n")
+        self.dataset = {}  # Reset the dataset for the test
+        self.dataset[os.path.basename(test_file)[:-4]] = data
+        print(f"Dataset Keys: {list(self.dataset.keys())}")
+        for key in self.dataset:
+            print(f" - {key}: Type = {type(self.dataset[key])}")
+            for k in self.dataset[key]:
+                print(f" - {k}: Shape = {self.dataset[key][k].shape}")
+                
+                
+                
+        print("_____________________________________________")
+        # Return the sorted data
+        print("\nFinal Data:")
+        final_data = self.return_data()
+        print(f"Type: {type(final_data)}")
+        print(f"Number of Entries: {len(final_data)}")
+        for key in final_data:
+            print(f" - {key}: Type = {type(final_data[key])}, Keys = {list(final_data[key].keys())}")
+
+
+
+
+
+
+# Use the local folder name instead of the full path
+folder_name = "Ne"
+A = EISCATDataSorter(folder_name)
+
+A.sort_data()
+
+data = A.return_data()
+
+a = data['2018-11-10']
+
+z  =  a['r_h']
+ne =  a['r_param']
+
+
+plt.plot(ne[:, 0:6], z[:, 0:6])
+plt.show()
+
+
+
+# A.test_dataflow()
 
 
 
