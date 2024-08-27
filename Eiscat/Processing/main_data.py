47729f16a4ab397d2bac8a7de056bce302b832f0
Kian Sartipzadeh <kian0902@hotmail.com>
2024-08-27 18:16:48 +0200

    Added some plotting code for vizualisation for only one instance (day) of using averaging.

--- Eiscat/Processing/main_data.py
+++ Eiscat/Processing/main_data.py
@@ -7,13 +7,14 @@ Created on Tue Aug 20 14:13:45 2024
 
 
 from sorting_data import EISCATDataSorter
+from average_data import EISCATAverager
 import matplotlib.pyplot as plt
-
+import numpy as np
 # Use the local folder name instead of the full path
 folder_name = "Ne_vhf"
 A = EISCATDataSorter(folder_name)
 
-A.sort_data(save_data=True, save_filename="Ne_vhf_ready_curvefit")
+# A.sort_data(save_data=True, save_filename="Ne_vhf_ready_curvefit")
 
 # data = A.return_data()
 
@@ -29,17 +30,46 @@ A.sort_data(save_data=True, save_filename="Ne_vhf_ready_curvefit")
 
 # a = data['2018-11-10']
 
-# z  =  a['r_h']
-# ne =  a['r_param']
 
 
-# A.test_dataflow()
+data = A.test_dataflow(return_data=True)
+
+t  = data['2018-11-10']['r_time']
+z  = data['2018-11-10']['r_h'].flatten()
+ne = data['2018-11-10']['r_param']
+
+print(t.shape, z.shape, ne.shape)
+
+tt = np.arange(len(t))
+print(tt.shape)
 
 
 
 
+plt.pcolormesh(tt, z, ne, shading='nearest',cmap='turbo')
+plt.clim(1e9, 5e11)
+plt.colorbar()
+plt.show()
 
-# plt.plot(ne[:, 0:12], z[:, 0:12])
+
+B = EISCATAverager(data['2018-11-10'])
+data_avg = B.average_over_period()
+
+
+t_avg  =  data_avg['r_time']
+z_avg  =  data_avg['r_h']
+ne_avg =  data_avg['r_param']
+
+
+tt_avg = np.arange(len(t_avg))
+
+plt.pcolormesh(tt_avg, z_avg, ne_avg, shading='nearest',cmap='turbo')
+plt.clim(1e9, 5e11)
+plt.colorbar()
+plt.show()
+
+# plt.plot(ne[:, 10:13], z)
+# plt.xscale('log')
 # plt.show()
 
 
