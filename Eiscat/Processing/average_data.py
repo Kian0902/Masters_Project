a957db81d4788f571c7a40085d87b5b1ad6fb6ee
Kian Sartipzadeh <kian0902@hotmail.com>
2024-08-27 16:00:57 +0200

    Made new python file for averaging data code. The purpose is to average the data over a 15 minute period.

---
+++ Eiscat/Processing/average_data.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Aug 27 15:58:50 2024
+
+@author: Kian Sartipzadeh
+"""
+
+
+
+
+import numpy as np
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
