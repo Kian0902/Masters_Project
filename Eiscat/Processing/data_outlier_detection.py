11c4c3e62a50a41e39331b1bdc284b442ed6395d
Kian Sartipzadeh <kian0902@hotmail.com>
2024-08-28 12:03:17 +0200

    Made class'OutlierDetection' which initializes with the input data. At this time, it has an unfinished method called 'detect_outliers'.

--- Eiscat/Processing/data_outlier_detection.py
+++ Eiscat/Processing/data_outlier_detection.py
@@ -9,14 +9,44 @@ Created on Wed Aug 28 11:04:56 2024
 
 
 
-
-
-
-
-
-
-
-
+class OutlierDetection:
+    """
+    Class for detecting outliers present in EISCAT data.
+    """
+    def __init__(self, data: dict):
+        """
+        Initialize with data.
+        
+        Attributes (type)    | DESCRIPTION
+        ------------------------------------------------
+        data (dict)          | Dictionary containing the NaN filtered EISCAT data
+        """
+        self.data = data
+    
+    
+    
+    
+    
+        
+    def detect_outliers(self):
+        
+        
+        
+        
+        r_time  = self.data['r_time']
+        r_h     = self.data['r_h']
+        r_param = self.data['r_param']
+        r_error = self.data['r_error']
+        
+        
+        print(r_time.shape)
+        print(r_h.shape)
+        print(r_param.shape)
+        print(r_error.shape)
+        
+        
+        
+        
 
 
 
