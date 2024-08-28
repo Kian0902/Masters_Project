902e42745be17056dab97be9a73a7487b16467c6
Kian Sartipzadeh <kian0902@hotmail.com>
2024-08-28 12:28:54 +0200

    Tested z-score method for outlier detection which was successful.

--- Eiscat/Processing/data_outlier_detection.py
+++ Eiscat/Processing/data_outlier_detection.py
@@ -7,24 +7,29 @@ Created on Wed Aug 28 11:04:56 2024
 
 import numpy as np
 from scipy.stats import zscore
-
+import matplotlib.pyplot as plt
 
 class OutlierDetection:
     """
     Class for detecting outliers present in EISCAT data.
     """
-    def __init__(self, data: dict):
+    def __init__(self, dataset: dict):
         """
-        Initialize with data.
+        Initialize with dataset containing EISCAT dat from one day.
         
-        Attributes (type)    | DESCRIPTION
+        Attributes (type) | DESCRIPTION
         ------------------------------------------------
-        data (dict)          | Dictionary containing the NaN filtered EISCAT data
+        dataset (dict)    | Dictionary containing the NaN filtered EISCAT data
         """
-        self.data = data
+        self.dataset = dataset
     
     
     
+    
+    def get_zscore(self, data: np.array, threshold: int=3):
+        z_score = zscore(data, axis=0)
+        outliers = np.abs(z_score) > threshold
+        return outliers
     
     
         
@@ -33,10 +38,10 @@ class OutlierDetection:
         
         
         
-        r_time  = self.data['r_time']
-        r_h     = self.data['r_h']
-        r_param = self.data['r_param']
-        r_error = self.data['r_error']
+        r_time  = self.dataset['r_time']
+        r_h     = self.dataset['r_h']
+        r_param = self.dataset['r_param']
+        r_error = self.dataset['r_error']
         
         print('\n')
         print(f'r_time:  {r_time.shape}')
@@ -46,7 +51,17 @@ class OutlierDetection:
         print('\n')
         
         
+        ne = r_param[:, 160:171]
         
+        plt.plot(ne, r_h.flatten())
+        plt.show()
+        
+        z_scores = self.get_zscore(ne)
+        
+        print(z_scores)
+        
+        # for i in np.arange(0, len(z_scores)):
+        #     print(ne[i], z_scores[i])
         
         
         
