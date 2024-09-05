ed6c1a9833a6f11e287cfd5c1728a84b94e442c9
Kian Sartipzadeh <kian0902@hotmail.com>
2024-09-05 16:02:50 +0200

    Made new python program for sorting txt file with all ionosonde data into monthly txt files with the corresponding data.

---
+++ Ionogram/sort_data.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep  5 15:55:21 2024
+
+@author: Kian Sartipzadeh
+"""
+
+
+
+
+
+import numpy as np
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
