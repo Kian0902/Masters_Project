7919b194635c199716870ce85e16bb54982e3514
Kian Sartipzadeh <kian0902@hotmail.com>
2024-09-10 11:57:08 +0200

    Generated gaussian data.

--- Eiscat/Curvefitting/curvefit_lmfit.py
+++ Eiscat/Curvefitting/curvefit_lmfit.py
@@ -14,24 +14,27 @@ from lmfit import Model
 
 
 
-
-
 def gaussian(x, amp, mean, sig):
     return amp * np.exp(-(x - mean) ** 2 / (2 * sig ** 2))
 
 
+true_amp  = 5.0
+true_mean = 0.0
+true_sig  = 1.0
+
+x = np.linspace(-5, 5, 1000)
+
+noise = np.random.normal(scale=0.2, size=x.size)
+
+y = gaussian(x, true_amp, true_mean, true_sig) + noise
 
 
-
-
-
-
-
-
-
-
-
-
+# Plot the synthetic data
+plt.scatter(x, y, label='Noisy data', color='blue')
+plt.xlabel('x')
+plt.ylabel('y')
+plt.legend()
+plt.show()
 
 
 
