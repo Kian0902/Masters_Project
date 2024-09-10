15f979330dbe0f637e855a8316441b912f0c1a7f
Kian Sartipzadeh <kian0902@hotmail.com>
2024-09-10 10:37:35 +0200

    Made a pytorch neural Network class with initialized parameters.

--- Eiscat/Curvefitting/curvefitting_NN.py
+++ Eiscat/Curvefitting/curvefitting_NN.py
@@ -13,7 +13,19 @@ import torch.nn as nn
 
 
 
-
+class Curvefit(nn.Module):
+    """
+    Class for curvefitting using a Neural Network. 
+    """
+    
+    def __init__(self):
+        super(Curvefit, self).__init__()
+        
+        # Setting up parameters to be optimized
+        self.HE_below = nn.Parameter(torch.tensor([np.log(5)], dtype=torch.float64), requires_grad=True)
+        self.HE_above = nn.Parameter(torch.tensor([np.log(10)], dtype=torch.float64), requires_grad=True)
+        self.HF_below = nn.Parameter(torch.tensor([np.log(25)], dtype=torch.float64), requires_grad=True)
+        self.HF_above = nn.Parameter(torch.tensor([np.log(40)], dtype=torch.float64), requires_grad=True)
 
 
 
