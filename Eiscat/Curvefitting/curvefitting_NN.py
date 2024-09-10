6908670be381d82fda0c776240adbef133d9815a
Kian Sartipzadeh <kian0902@hotmail.com>
2024-09-10 10:39:37 +0200

    Added a 'forward' method which calculated the double chapman model.

--- Eiscat/Curvefitting/curvefitting_NN.py
+++ Eiscat/Curvefitting/curvefitting_NN.py
@@ -28,9 +28,31 @@ class Curvefit(nn.Module):
         self.HF_above = nn.Parameter(torch.tensor([np.log(40)], dtype=torch.float64), requires_grad=True)
 
 
+    def forward(self, x, nE_peak, nF_peak, zE_peak, zF_peak):
 
 
-
-
+        # Converting numpy.float64 to torch.float64
+        nE_peak = torch.tensor([nE_peak], dtype=torch.float64)
+        nF_peak = torch.tensor([nF_peak], dtype=torch.float64)
+        
+        zE_peak = torch.tensor([zE_peak], dtype=torch.float64)
+        zF_peak = torch.tensor([zF_peak], dtype=torch.float64)
+        
+        # Getting rid of log
+        HE_below = torch.exp(self.HE_below)
+        HE_above = torch.exp(self.HE_above)
+        HF_below = torch.exp(self.HF_below)
+        HF_above = torch.exp(self.HF_above)
+        
+        # Appending scale heights corresponding on altitude
+        HE = torch.where(x < zE_peak, HE_below, HE_above)
+        HF = torch.where(x < zF_peak, HF_below, HF_above)
+        
+        
+        # Defining chapman electron density profile for both regions
+        neE = nE_peak * torch.exp(1 - (x - zE_peak) / HE - torch.exp(-((x - zE_peak) / HE))) 
+        neF = nF_peak * torch.exp(1 - (x - zF_peak) / HF - torch.exp(-((x - zF_peak) / HF)))
+        
+        return neE + neF
 
 
