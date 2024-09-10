2a967f1016429aaf03123d02d6b9c534a0b32fda
Kian Sartipzadeh <kian0902@hotmail.com>
2024-09-10 12:03:48 +0200

    Tested lmfit curvefitting model on gaussian function. Now, I will proceed with curvefitting the double chapman function.

--- Eiscat/Curvefitting/curvefit_lmfit.py
+++ Eiscat/Curvefitting/curvefit_lmfit.py
@@ -13,7 +13,7 @@ from lmfit import Model
 
 
 
-
+# Defining function to be curvefitted
 def gaussian(x, amp, mean, sig):
     return amp * np.exp(-(x - mean) ** 2 / (2 * sig ** 2))
 
@@ -26,6 +26,7 @@ x = np.linspace(-5, 5, 1000)
 
 noise = np.random.normal(scale=0.2, size=x.size)
 
+np.random.seed(42)
 y = gaussian(x, true_amp, true_mean, true_sig) + noise
 
 
@@ -39,6 +40,31 @@ plt.show()
 
 
 
+# LMFIT model
+
+#Create fitting model
+gaussian_model = Model(gaussian)
+
+
+# Init params
+params = gaussian_model.make_params(amp=4, mean=0, sig=2)
+
+
+# Firt model to data
+result = gaussian_model.fit(y, params, x=x)
+
+
+print(result.fit_report())
+
+
+
+# Plot the fitted curve
+plt.scatter(x, y, label='Noisy data', color='blue')
+plt.plot(x, result.best_fit, label='Best fit', color='red')
+plt.xlabel('x')
+plt.ylabel('y')
+plt.legend()
+plt.show()
 
 
 
