d9e0a78132c526b579e5dfd3352b6204c4fee9b9
Kian Sartipzadeh <kian0902@hotmail.com>
2024-09-10 13:04:52 +0200

    Made python file with single class for choosing curvefitting model (scipy, lmfit, torch).

---
+++ Eiscat/Curvefitting/curvefitting_models.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 10 13:02:05 2024
+
+@author: Kian Sartipzadeh
+"""
+
+
+import numpy as np
+import matplotlib.pyplot as plt
+from lmfit import Model
+
+
+
+class Curvefitting:
+    """
+    Class for choosing between 3 different curvefitting models:
+    - SciPy
+    - lmfit
+    - torch
+    """
+    def __init__(self):
+        ...
+    
+    def curvefit_scipy(self):
+        ...
+        
+    def curvefit_lmfit(self):
+        ...
+    
+    def curvefit_neural_network(self):
+        ...
+        
