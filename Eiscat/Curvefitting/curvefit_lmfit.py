921f61dfc4e308bbe7f192a839cc893135afbe9d
Kian Sartipzadeh <kian0902@hotmail.com>
2024-09-10 12:36:07 +0200

    Tested lmfit on a simple chapman model function with one scaleheight.

--- Eiscat/Curvefitting/curvefit_lmfit.py
+++ Eiscat/Curvefitting/curvefit_lmfit.py
@@ -13,69 +13,53 @@ from lmfit import Model
 
 
 
-# Defining function to be curvefitted
-def gaussian(x, amp, mean, sig):
-    return amp * np.exp(-(x - mean) ** 2 / (2 * sig ** 2))
+# Defining simple chapman model function
+def chapman(z, z_peak, ne_peak, H):
+    
+    y = (z - z_peak)/H
+    ne = ne_peak * np.exp(1 - y - np.exp(-y))
+    return ne
 
 
-true_amp  = 5.0
-true_mean = 0.0
-true_sig  = 1.0
 
-x = np.linspace(-5, 5, 1000)
-
-noise = np.random.normal(scale=0.2, size=x.size)
-
-np.random.seed(42)
-y = gaussian(x, true_amp, true_mean, true_sig) + noise
+x = np.linspace(90, 300, 22)
+y = chapman(x, z_peak=130, ne_peak=1e8, H=25)
 
 
 # Plot the synthetic data
-plt.scatter(x, y, label='Noisy data', color='blue')
-plt.xlabel('x')
-plt.ylabel('y')
+plt.plot(y, x, label='Chapman', color='C0')
+plt.xlabel('Ne')
+plt.ylabel('z')
 plt.legend()
 plt.show()
 
 
 
 
-# LMFIT model
-
-#Create fitting model
-gaussian_model = Model(gaussian)
 
 
-# Init params
-params = gaussian_model.make_params(amp=4, mean=0, sig=2)
+curvefit_model = Model(chapman)
+params = curvefit_model.make_params(H=10)
 
+params.add('z_peak', value=130, vary=False)
+params.add('ne_peak', value=1e8, vary=False)
 
-# Firt model to data
-result = gaussian_model.fit(y, params, x=x)
+result = curvefit_model.fit(y, params, z=x)
 
+y_fit = result.best_fit
 
 print(result.fit_report())
 
 
 
-# Plot the fitted curve
-plt.scatter(x, y, label='Noisy data', color='blue')
-plt.plot(x, result.best_fit, label='Best fit', color='red')
-plt.xlabel('x')
-plt.ylabel('y')
+
+# Plot the synthetic data
+plt.plot(y, x, label='Chapman', color='C0')
+plt.plot(y_fit, x, label='Curvefit', color='C1')
+plt.xlabel('Ne')
+plt.ylabel('z')
 plt.legend()
 plt.show()
-
-
-
-
-
-
-
-
-
-
-
 
 
 
