bea5e353305e2f793741e3ca4934219cc1065acd
Kian Sartipzadeh <kian0902@hotmail.com>
2024-09-11 11:40:40 +0200

    Made a wrapper function for the double chapman model

--- Eiscat/Curvefitting/curvefitting_models.py
+++ Eiscat/Curvefitting/curvefitting_models.py
@@ -27,6 +27,13 @@ class Curvefitting:
                                    'NN': self.curvefit_neural_network}
         
     
+    def _double_chapman_wrapper(self, z, HEd, HEu, HFd, HFu, zE_peak, zF_peak, neE_peak, neF_peak):
+        """
+        Wrapper function for the double chapman.
+        """
+        return self.double_chapman(z, HEd, HEu, HFd, HFu, zE_peak, zF_peak, neE_peak, neF_peak)
+    
+    
     def double_chapman(self, z, HEd, HEu, HFd, HFu, zE_peak, neE_peak, zF_peak, neF_peak):
         """
         Function for the double chapman electron density model.
@@ -49,10 +56,8 @@ class Curvefitting:
         ------------------------------------------------
         neE + neF     | Double Chapman electron density.
         """
-        
-        
-        HE = np.where(z <= zE_peak, HEd, HEu)
-        HF = np.where(z <= zF_peak, HFd, HFu)
+        HE = np.where(z <= zE_peak, HEd, HEu)   # E-reg scale-height
+        HF = np.where(z <= zF_peak, HFd, HFu)   # F-reg scale-height
         
         neE = neE_peak * np.exp(1 - ((z - zE_peak)/HE) - np.exp(-((z - zE_peak)/HE)))
         neF = neF_peak * np.exp(1 - ((z - zF_peak)/HF) - np.exp(-((z - zF_peak)/HF)))
@@ -68,3 +73,56 @@ class Curvefitting:
     def curvefit_neural_network(self):
         ...
         
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
