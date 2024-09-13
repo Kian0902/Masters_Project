7ac9dc994839a5ce10b08adb7e626d808d50fa38
Kian Sartipzadeh <kian0902@hotmail.com>
2024-09-13 12:51:22 +0200

    Made class for evaluation methods.

--- Eiscat/Curvefitting/curvefitting_evaluation.py
+++ Eiscat/Curvefitting/curvefitting_evaluation.py
@@ -8,10 +8,24 @@ Created on Fri Sep 13 12:19:21 2024
 
 
 
+class CurvefittingEvaluation:
+    """
+    Class for evaluating the double chapman curvefitting model performance.
+    
+    Methods:
+        - residual_norm
+        - chi_square
+        - rmse
+    """
+    def __init__(self, dataset, fitted_dataset):
+        
+        self.dataset = dataset
+        self.fitted_dataset = fitted_dataset
 
-
-
-
+    
+    
+    def residual_norm(self, data):
+        ...
 
 
 
