1231b5a1df3f563b66a6e091b9945e877c345296
Kian Sartipzadeh <kian0902@hotmail.com>
2024-09-13 14:53:12 +0200

    Made plotting for evaluation.

--- Eiscat/Curvefitting/curvefitting_evaluation.py
+++ Eiscat/Curvefitting/curvefitting_evaluation.py
@@ -11,6 +11,9 @@ import matplotlib.pyplot as plt
 
 from datetime import datetime, timedelta
 from matplotlib.dates import DateFormatter
+import matplotlib.gridspec as gridspec
+import matplotlib.colors as mcolors
+
 
 
 class CurvefittingEvaluation:
@@ -52,10 +55,41 @@ class CurvefittingEvaluation:
         residuals = (ne - ne_fit)/ne_error
         
         
-        print(t.shape, z.shape, residuals.shape, ne_error.shape)
-        
         if plot:
-            plt.pcolormesh(t, z, residuals)
+            vmin = 1e9
+            vmax = 1e12
+            
+            
+            # Create the figure and GridSpec layout
+            fig = plt.figure(figsize=(25/2.54, 20/2.54))
+            gs = gridspec.GridSpec(2, 2, width_ratios=[2, 2], height_ratios=[1, 1])
+            
+            # ne plot
+            ax0 = fig.add_subplot(gs[0, 0])
+            c0  = ax0.pcolormesh(t, z, ne, norm=mcolors.LogNorm(vmin=vmin, vmax=vmax), cmap='turbo')
+            ax0.set_title("True")
+            ax0.xaxis.set_major_formatter(DateFormatter('%d %H:%M'))
+            ax0.set_xticklabels([])
+            fig.colorbar(c0, ax=ax0)
+            
+            # ne_fit plot
+            ax1 = fig.add_subplot(gs[1, 0])
+            c1  = ax1.pcolormesh(t, z, ne_fit, norm=mcolors.LogNorm(vmin=vmin, vmax=vmax), cmap='turbo')
+            ax1.set_title("Fitted")
+            ax1.xaxis.set_major_formatter(DateFormatter('%d %H:%M'))
+            fig.colorbar(c1, ax=ax1)
+            
+            # residual plot
+            ax2 = fig.add_subplot(gs[:, 1])  # spans both rows
+            c2  = ax2.pcolormesh(t, z, residuals, cmap='bwr', vmin=-10.0, vmax=10.0)
+            ax2.set_title("Normalized Residuals")
+            ax2.xaxis.set_major_formatter(DateFormatter('%d %H:%M'))
+            fig.colorbar(c2, ax=ax2)
+            
+            # Adjust layout to prevent overlapping
+            fig.tight_layout()
+            fig.autofmt_xdate()
+            
             plt.show()
         
         return residuals
@@ -85,7 +119,7 @@ file_fit = "Ne_vhf_avg_lmfit_curvefits"
 X_org = import_file(file_org)
 X_fit = import_file(file_fit)
 
-key_choise = list(X_org.keys())[:1]
+key_choise = list(X_org.keys())[:]
 
 
 x_org = {key: X_org[key] for key in key_choise}['2018-11-10']
