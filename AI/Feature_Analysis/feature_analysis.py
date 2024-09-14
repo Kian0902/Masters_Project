d08560a67cc4c81717ff60278477065c71e10504
Kian Sartipzadeh <kian0902@hotmail.com>
2024-09-14 16:15:22 +0200

    Added method to get correlation_matrix for the features.

--- AI/Feature_Analysis/feature_analysis.py
+++ AI/Feature_Analysis/feature_analysis.py
@@ -6,7 +6,7 @@ Created on Sat Sep 14 16:07:10 2024
 """
 
 
-
+import numpy as np
 
 
 class FeatureAnalysis:
@@ -15,10 +15,19 @@ class FeatureAnalysis:
     def __init__(self, dataset):
         
         self.dataset = dataset
-
     
-
-
+    
+    
+    def correlation_matrix(self):
+        
+        corr_matrix = np.corrcoef(self.dataset, rowvar=False)
+        
+        return corr_matrix
+    
+    
+    
+    
+    
 
 
 
