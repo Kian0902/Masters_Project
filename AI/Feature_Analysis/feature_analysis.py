6ba0369f0b1cd7274d0789b72b81ffbd724c071f
Kian Sartipzadeh <kian0902@hotmail.com>
2024-09-14 16:56:40 +0200

    Added option for plotting correlation matrix.

--- AI/Feature_Analysis/feature_analysis.py
+++ AI/Feature_Analysis/feature_analysis.py
@@ -7,24 +7,40 @@ Created on Sat Sep 14 16:07:10 2024
 
 
 import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+
 
 
 class FeatureAnalysis:
     
     
-    def __init__(self, dataset):
+    def __init__(self, dataset, feature_names):
         
         self.dataset = dataset
+        self.feature_names = feature_names
     
     
-    
-    def correlation_matrix(self):
+    def correlation_matrix(self, plot=False):
         
         corr_matrix = np.corrcoef(self.dataset, rowvar=False)
         
+        if plot:
+            self.plot_correlation_matrix()
+            
         return corr_matrix
     
     
+    def plot_correlation_matrix(self):
+        
+        
+        corr_matrix = self.correlation_matrix()
+        plt.figure(figsize=(12, 10))
+        sns.heatmap(corr_matrix, annot=True, fmt=".2f", cmap='coolwarm', square=True, linewidths=.5, 
+                    xticklabels=self.feature_names, yticklabels=self.feature_names)
+        plt.title('Correlation Matrix')
+        plt.show()
     
     
     
