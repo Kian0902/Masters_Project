593350b3edbe97235f50507982c06da8098817b6
Kian Sartipzadeh <kian0902@hotmail.com>
2024-09-14 14:34:40 +0200

    Tried to import csv file names with glob library.

--- AI/Feature_Analysis/sorting_files.py
+++ AI/Feature_Analysis/sorting_files.py
@@ -2,6 +2,41 @@
 """
 Created on Sat Sep 14 14:04:36 2024
 
-@author: kian0
+@author: Kian Sartipzadeh
 """
+
+
+
+
+import os
+import glob
+import pandas as pd
+
+
+
+
+folder_path = "ionograms_1D"
+
+all_files = glob.glob(os.path.join(folder_path, "*.csv"))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
