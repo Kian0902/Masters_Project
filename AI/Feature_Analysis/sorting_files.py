ee627fad3116662a5a13e6e5bda6bf07159ae3e7
Kian Sartipzadeh <kian0902@hotmail.com>
2024-09-14 15:41:46 +0200

    Updated code that gathers all the csv files into one single file, by adding headers for the 19 space physics features.

--- AI/Feature_Analysis/sorting_files.py
+++ AI/Feature_Analysis/sorting_files.py
@@ -14,28 +14,34 @@ import pandas as pd
 
 
 
+custom_header=['DoY/366', 'ToD/1440', 'Solar_Zenith/44', 'Kp', 'R', 'Dst',
+               'ap', 'F10_7', 'AE', 'AL', 'AU', 'PC_potential', 'Lyman_alpha',
+               'Bx', 'By', 'Bz', 'dBx', 'dBy', 'dBz']
+
 
 folder_path = "ionograms_1D"
 
-all_files = glob.glob(os.path.join(folder_path, "*.csv"))
 
-
-def sort():
-    with open('ionograms1D.csv', 'w') as outfile:
-        for i, file in enumerate(all_files):
-            
-            if i % 100 == 0:
-                print(f'{i}/{len(all_files)}')
-            
-            df = pd.read_csv(file)
-            if i==0:
-                df.to_csv(outfile, index=False)
-            else:
-                df.to_csv(outfile, index=False, header=False)
+df_list = []
 
 
 
+for i, filename in enumerate(os.listdir(folder_path)):
+    if filename.endswith('.csv'):
+        if i % 1000 == 0:
+            print(f'{i}/{len(os.listdir(folder_path))}')
+        
+        file_path = os.path.join(folder_path, filename)
+        
+        
+        df = pd.read_csv(file_path, header=None, skiprows=1, nrows=19)
+        
+        df = df.T
+        
+        df_list.append(df)
 
+combined_df = pd.concat(df_list, axis=0)
+combined_df.to_csv('spacephysics_19features.csv', index=False, header=custom_header)
 
 
 
