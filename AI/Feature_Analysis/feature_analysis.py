cf0e77baadf7425f8512c8bc3917bc7c8d266ba2
Kian Sartipzadeh <kian0902@hotmail.com>
2024-09-14 16:10:48 +0200

    Made python file for writing code for feature analysis.

---
+++ AI/Feature_Analysis/feature_analysis.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Sep 14 16:07:10 2024
+
+@author: Kian Sartipzadeh
+"""
+
+
+
+
+
+class FeatureAnalysis:
+    
+    
+    def __init__(self, dataset):
+        
+        self.dataset = dataset
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- AI/Feature_Analysis/main_features.py
+++ AI/Feature_Analysis/main_features.py
@@ -5,14 +5,23 @@ Created on Sat Sep 14 13:46:08 2024
 @author: Kian Sartipzadeh
 """
 
+import time
+start_time = time.time()
 
 
 import numpy as np
+import pandas as pd
 import matplotlib.pyplot as plt
 
 
 
 
+# Extracting the header
+with open("spacephysics_19features.csv", "r") as f:
+    header = f.readline().strip().split(",")
+
+# Reading 19 space physics features
+data = np.genfromtxt("spacephysics_19features.csv", delimiter=",", skip_header=0)
 
 
 
@@ -47,6 +56,19 @@ import matplotlib.pyplot as plt
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+end_time = time.time()
+print(f"Time taken to run the line: {end_time - start_time:.6f} seconds")
 
 
 
