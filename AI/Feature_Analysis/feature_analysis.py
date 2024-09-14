b5528426fbaee7358d01c4b9ababadc9f2f90b6b
Kian Sartipzadeh <kian0902@hotmail.com>
2024-09-14 17:25:23 +0200

    Added method for plotting correlogram. Currently problematic as it cannout handle 19 features and 80k samples

--- AI/Feature_Analysis/feature_analysis.py
+++ AI/Feature_Analysis/feature_analysis.py
@@ -7,6 +7,7 @@ Created on Sat Sep 14 16:07:10 2024
 
 
 import numpy as np
+import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 
@@ -22,31 +23,43 @@ class FeatureAnalysis:
         self.feature_names = feature_names
     
     
-    def correlation_matrix(self, plot=False):
-        
+    def correlation_matrix(self, plot_correlation_matrix=False, plot_correlogram=False):
         corr_matrix = np.corrcoef(self.dataset, rowvar=False)
         
-        if plot:
+        if plot_correlation_matrix:
             self.plot_correlation_matrix()
-            
+        
+        if plot_correlogram:
+            self.plot_correlogram()
+        
         return corr_matrix
     
     
     def plot_correlation_matrix(self):
         
-        
+        # Calculating correlation matrix
         corr_matrix = self.correlation_matrix()
+        
         plt.figure(figsize=(12, 10))
         sns.heatmap(corr_matrix, annot=True, fmt=".2f", cmap='coolwarm', square=True, linewidths=.5, 
                     xticklabels=self.feature_names, yticklabels=self.feature_names)
         plt.title('Correlation Matrix')
         plt.show()
-    
-    
-    
 
 
 
+    def plot_correlogram(self):
+        print("h")
+        
+        # Convert the dataset to a DataFrame for use with Seaborn
+        df = pd.DataFrame(self.dataset, columns=self.feature_names)
+        
+        
+        print("d")
+        
+        sns.pairplot(df, kind='scatter', diag_kind='kde')
+        plt.suptitle('Correlogram')
+        plt.show()
 
 
 
