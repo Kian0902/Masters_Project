8d50904e9158a4d4de60c37539219a45cc6a6295
Kian Sartipzadeh <kian0902@hotmail.com>
2024-09-16 15:10:20 +0200

    Final commit before endign session.

--- AI/Feature_Analysis/Prep_Dataset/sorting_dates.py
+++ AI/Feature_Analysis/Prep_Dataset/sorting_dates.py
@@ -6,6 +6,8 @@ Created on Mon Sep 16 10:27:24 2024
 """
 
 
+# 20180917_1615
+
 import pickle
 import numpy as np
 
@@ -15,6 +17,10 @@ import numpy as np
 
 
 
+filename = "Ne_vhf_avg_lmfit_curvefits"
+
+with open(filename, 'rb') as f:
+    dataset = pickle.load(f)
 
 
 
