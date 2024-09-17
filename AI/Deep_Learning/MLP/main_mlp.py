5fa7704da17b81011aed6b1e3f8d75cc351f9621
Kian Sartipzadeh <kian0902@hotmail.com>
2024-09-17 15:14:58 +0200

    Made code to plot all samples.

--- AI/Deep_Learning/MLP/main_mlp.py
+++ AI/Deep_Learning/MLP/main_mlp.py
@@ -13,6 +13,7 @@ import torch.optim as optim
 from torch.utils.data import Dataset, DataLoader
 
 import numpy as np
+import matplotlib.pyplot as plt
 
 from sklearn.datasets import make_blobs
 from sklearn.model_selection import train_test_split
@@ -28,21 +29,19 @@ X = scaler.fit_transform(X)
 
 
 # Split the dataset into training and testing sets
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, stratify=y)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, stratify=y, random_state=1)
 
 
+plt.figure()
+plt.title("Blobs dataset")
+plt.scatter(X_train[:,0][y_train==0], X_train[:,1][y_train==0], color="C0", label="Class 1")
+plt.scatter(X_train[:,0][y_train==1], X_train[:,1][y_train==1], color="C1", label="Class 2")
+plt.xlabel("X1")
+plt.ylabel("X2")
+plt.legend()
+plt.show()
 
 
-
-
-
-# class MultiLayeredPerceptron(nn.Module):
-#     def __init__(self):
-#         super(MultiLayeredPerceptron, self).__init__()
-        
-#         self.FC1 = nn.Linear(19, 100)
-#         self.FC2 = nn.Linear(100, out_features)
-        
 
 
 
