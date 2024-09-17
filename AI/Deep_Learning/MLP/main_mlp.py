dedfc4698c93260d2c4a4566943172e6af27b3e9
Kian Sartipzadeh <kian0902@hotmail.com>
2024-09-17 15:04:01 +0200

    Imported the necessary libraries for pytorch mlp architecture.

--- AI/Deep_Learning/MLP/main_mlp.py
+++ AI/Deep_Learning/MLP/main_mlp.py
@@ -7,18 +7,16 @@ Created on Tue Sep 17 14:42:19 2024
 
 
 
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.utils.data import Dataset, DataLoader
 
+import numpy as np
 
-
-
-
-
-
-
-
-
-
-
+from sklearn.datasets import make_blobs
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
 
 
 
