b92183f00d302a2e6a49769e79ff7ed951d64c70
Kian Sartipzadeh <kian0902@hotmail.com>
2024-09-17 15:09:00 +0200

    Imported blobs dataset for future testing of network. This is to recap the use of pytorch.

--- AI/Deep_Learning/MLP/main_mlp.py
+++ AI/Deep_Learning/MLP/main_mlp.py
@@ -19,6 +19,33 @@ from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 
 
+X, y = make_blobs(n_samples=1000, centers=2, n_features=2)
+
+
+# Standardize
+scaler = StandardScaler()
+X = scaler.fit_transform(X)
+
+
+# Split the dataset into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, stratify=y)
+
+
+
+
+
+
+
+# class MultiLayeredPerceptron(nn.Module):
+#     def __init__(self):
+#         super(MultiLayeredPerceptron, self).__init__()
+        
+#         self.FC1 = nn.Linear(19, 100)
+#         self.FC2 = nn.Linear(100, out_features)
+        
+
+
+
 
 
 
