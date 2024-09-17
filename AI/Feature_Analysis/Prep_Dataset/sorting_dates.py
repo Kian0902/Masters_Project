402d56fd79a32df0fbec347b618f3add1403ae87
Kian Sartipzadeh <kian0902@hotmail.com>
2024-09-17 13:22:20 +0200

    Began making code for creating samples out of processed, filtered, averaged and curvefitted dataset.

--- AI/Feature_Analysis/Prep_Dataset/sorting_dates.py
+++ AI/Feature_Analysis/Prep_Dataset/sorting_dates.py
@@ -9,18 +9,15 @@ Created on Mon Sep 16 10:27:24 2024
 # 20180917_1615
 
 import pickle
-import numpy as np
 
 
-
-
-
-
-
+# Name of file containing radar data
 filename = "Ne_vhf_avg_lmfit_curvefits"
 
+# Opening file
 with open(filename, 'rb') as f:
-    dataset = pickle.load(f)
+    dataset = pickle.load(f)  # Loading dictionary
+    
 
 
 
