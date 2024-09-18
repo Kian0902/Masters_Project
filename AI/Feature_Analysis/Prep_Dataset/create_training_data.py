d5cac434a80ce68c7e47d36db4701940c93a98b5
Kian Sartipzadeh <kian0902@hotmail.com>
2024-09-18 17:58:08 +0200

    Updated program to match files names more efficiently.

--- AI/Feature_Analysis/Prep_Dataset/create_training_data.py
+++ AI/Feature_Analysis/Prep_Dataset/create_training_data.py
@@ -6,62 +6,85 @@ Created on Tue Sep 18 14:42:19 2024
 """
 
 
-
 import os
+import numpy as np
 import pandas as pd
-
 
 
 
 def list_csv_files(folder_path):
     return [f for f in os.listdir(folder_path) if f.endswith('.csv')]
 
+def get_filename_without_extension(filename):
+    return os.path.splitext(filename)[0]
 
-def match_files(A_files, B_files):
+
+radar_names = list_csv_files("EISCAT_samples")
+satellite_names = list_csv_files("SP19_samples")
+
+
+# Extract base filenames without ".csv"
+radar_filenames = set(get_filename_without_extension(f) for f in radar_names)
+satellite_filenames = set(get_filename_without_extension(f) for f in satellite_names)
+
+# Find the matching filenames
+matching_filenames = sorted(list(satellite_filenames.intersection(radar_filenames)))
+
+
+
+
+
+radar_data_list = []
+satellite_data_list = []
+
+for filename in matching_filenames:
+    radar_file_path = os.path.join("EISCAT_samples", filename + ".csv")
+    satellite_file_path = os.path.join("SP19_samples", filename + ".csv")
     
     
-    radar_dates = {f.split('.')[0]: f for f in A_files}
-    satellite_dates = {f.split('.')[0]: f for f in B_files}
+    radar_data = np.loadtxt(radar_file_path, delimiter=',')
+    satellite_data = np.loadtxt(satellite_file_path, delimiter=',')
     
     
-    matched_pairs = []
-    for date in satellite_dates.keys():
-        if date in radar_dates:
-            matched_pairs.append((satellite_dates[date], radar_dates[date]))
-    
-    return matched_pairs
+    radar_data_list.append(radar_data)
+    satellite_data_list.append(satellite_data)
 
 
-
-def load_data_pairs(radar_folder, satellite_folder, matched_pairs):
-    paired_data = []
-    
-    i=1
-    for satellite_file, radar_file in matched_pairs:
-        satellite_data = pd.read_csv(os.path.join(satellite_folder, satellite_file))
-        radar_data = pd.read_csv(os.path.join(radar_folder, radar_file))
-        paired_data.append((satellite_data, radar_data))
-        
-        if i % 100 ==0:
-            print(i)
-        
-        i+=1
-        
-    return paired_data
-
-eiscat_files = list_csv_files("EISCAT_samples")
-sp19_files = list_csv_files("SP19_samples")
-
-pairs = match_files(eiscat_files, sp19_files)
+radar_data = np.array(radar_data_list)
+satellite_data= np.array(satellite_data_list)
 
 
-dataset = load_data_pairs("EISCAT_samples", "SP19_samples", pairs)
+print(radar_data[0])
+print(satellite_data[0])
+
+
+np.save('radar_data.npy', radar_data)
+np.save('satellite_data.npy', satellite_data)
 
 
 
 
-print(len(pairs))
-print(len(dataset))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
