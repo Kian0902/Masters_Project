afeffbf533e3e10d236425a6f41802fff5a44d7f
Kian Sartipzadeh <kian0902@hotmail.com>
2024-09-18 13:18:08 +0200

    Made new python file for creating training pairs of EISCAT and SP19 samples

---
+++ AI/Feature_Analysis/Prep_Dataset/create_training_data.py
@@ -0,0 +1,58 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep 18 13:07:43 2024
+
+@author: Kian Sartipzadeh
+"""
+
+# SP19_samples
+# EISCAT_samples
+
+
+import os
+import numpy as np
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
