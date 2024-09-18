37d0496068a786608222fcc2d5700c727c4cbf55
Kian Sartipzadeh <kian0902@hotmail.com>
2024-09-18 13:28:34 +0200

    Added function for appending csv file names.

--- AI/Feature_Analysis/Prep_Dataset/create_training_data.py
+++ AI/Feature_Analysis/Prep_Dataset/create_training_data.py
@@ -5,3 +5,36 @@ Created on Wed Sep 18 13:21:16 2024
 @author: Kian Sartipzadeh
 """
 
+
+
+
+import os
+import pandas as pd
+
+
+
+def list_csv_files(folder_path):
+    
+    L = []
+    for f in os.listdir(folder_path):
+        if f.endswith('.csv'):
+            L.append(f)
+        else:
+            pass
+    
+    return L
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
