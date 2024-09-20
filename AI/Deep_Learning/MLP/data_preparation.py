ff1b880eb53c911f551a7cd564f7d23e1ed89814
Kian Sartipzadeh <kian0902@hotmail.com>
2024-09-20 18:47:06 +0200

    Tested MLP on dataset. Problematic training process as the outputs become nan.

--- AI/Deep_Learning/MLP/data_preparation.py
+++ AI/Deep_Learning/MLP/data_preparation.py
@@ -8,21 +8,27 @@ Created on Fri Sep 20 13:21:45 2024
 
 
 
+import numpy as np
+
+
+data_sp19 = np.load('sp19_data.npy')
+data_eiscat = np.load('eiscat_data.npy')
 
 
 
 
 
+print(np.amin(data_sp19), np.amax(data_sp19))
+print(np.amin(data_eiscat), np.amax(data_eiscat))
 
 
 
+data_eiscat[data_eiscat < 1e5] = 10**5
 
 
 
-
-
-
-
+print(np.amin(data_sp19), np.amax(data_sp19))
+print(np.amin(data_eiscat), np.amax(data_eiscat))
 
 
 
--- AI/Deep_Learning/MLP/main_mlp.py
+++ AI/Deep_Learning/MLP/main_mlp.py
@@ -21,7 +21,6 @@ print(device)
 
 
 
-
 class StoreDataset(Dataset):
     def __init__(self, data, targets):
         self.data = torch.tensor(data, dtype=torch.float32)
@@ -37,212 +36,98 @@ class StoreDataset(Dataset):
 
 
 class MLP(nn.Module):
-    def __init__(self, in_dim, hidden_dim, out_dim):
+    def __init__(self, in_dim, out_dim):
         super(MLP, self).__init__()
         
         # MLP layers
-        self.FC1  = nn.Linear(in_dim, hidden_dim)
-        self.FC2  = nn.Linear(hidden_dim, hidden_dim)
-        self.FC3  = nn.Linear(hidden_dim, out_dim)
+        self.FC1  = nn.Linear(in_dim, 128)
+        self.FC2  = nn.Linear(128, 128)
+        self.FC3  = nn.Linear(128, 64)
+        self.FC4  = nn.Linear(64, out_dim)
+        
         
         # Activation
         self.relu = nn.ReLU()
-        self.softplus = nn.Softplus(beta=0.2, threshold=600)
-
+        self.softplus = nn.Softplus(beta=0.2, threshold=1)
+        
     
-    def forward(self, x):
-        # MLP forward pass
+    def double_chapman(self, x, z):
+        zE_peak, zF_peak, nE_peak, nF_peak, HE_below, HF_below, HE_above, HF_above = x.split(1, dim=1)
+        
+        # print(f"zE_peak:  min={torch.min(zE_peak)}, max={torch.max(zE_peak)}")
+        # print(f"zF_peak:  min={torch.min(zF_peak)}, max={torch.max(zF_peak)}")
+        # print(f"nE_peak:  min={torch.min(nE_peak)}, max={torch.max(nE_peak)}")
+        # print(f"nF_peak:  min={torch.min(nF_peak)}, max={torch.max(nF_peak)}")
+        # print(f"HE_below:  min={torch.min(HE_below)}, max={torch.max(HE_below)}")
+        # print(f"HF_below:  min={torch.min(HF_below)}, max={torch.max(HF_below)}")
+        # print(f"nE_peak:  min={torch.min(HE_above)}, max={torch.max(HE_above)}")
+        # print(f"nF_peak:  min={torch.min(HF_above)}, max={torch.max(HF_above)}")
+        # print("\n")
+        # print(zE_peak.shape)
+        # print(torch.min(zE_peak))
+        
+        
+        # Adding epsilon to avoid division by zero
+        HE = torch.where(z < zE_peak, HE_below, HE_above)
+        HF = torch.where(z < zF_peak, HF_below, HF_above)
+    
+        # Clamping to avoid overflow in torch.exp
+        neE = nE_peak * torch.exp(1 - ((z - zE_peak) / HE) - torch.exp(-((z - zE_peak) / HE)))
+        neF = nF_peak * torch.exp(1 - ((z - zF_peak) / HF) - torch.exp(-((z - zF_peak) / HF)))
+        # Calculate min and max for neE and neF
+        neE_min, neE_max = torch.min(neE), torch.max(neE)
+        neF_min, neF_max = torch.min(neF), torch.max(neF)
+        
+        # print(neE_min, neE_max)
+        # print(neF_min, neF_max)
+        return neE + neF
+        
+    
+    
+    def forward(self, x, z):
+
         x = self.FC1(x)
         x = self.relu(x)
         x = self.FC2(x)
         x = self.relu(x)
         x = self.FC3(x)
+        x = self.relu(x)
+        x = self.FC4(x)
         x = self.softplus(x)
+        
+        # Clamp parameters within specified ranges
+        # min_values = torch.tensor([80, 200, 3, 3, 3, 3, 3, 3], device=x.device)  # Set appropriate lower bounds for each parameter
+        # max_values = torch.tensor([200, 400, 15, 15, 100, 100, 100, 100], device=x.device)  # Set appropriate upper bounds for each parameter
+    
+        # x = torch.clamp(x, min=min_values, max=max_values)
+            
+        batch_siz1e = x.size(0)
+        # z = z.unsqueeze(0).expand(batch_size, -1).to(device)
+        
+        # chapman_output = self.double_chapman(x, z)
+        # print(torch.min(chapman_output, 0))
+        
         return x
-
-
-
-in_dim = 19
-hidden_dim = 20
-out_dim = 8
-
-model = MLP(in_dim, hidden_dim, out_dim)
-
-
-
-batch_size = 1
-dummy_x = torch.randn(batch_size, in_dim)
-
-
-
-output = model(dummy_x)
-
-
-print("Output shape:", output.shape)
-print("Output:", output)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# data_sp19 = np.load('sp19_data.npy')
-# data_eiscat = np.load('eiscat_data.npy')
-
-# X_train, X_test, y_train, y_test = train_test_split(data_sp19, data_eiscat, train_size=0.8, shuffle=False)
-
-# y_train[y_train < 10**5] = 10**5
-# y_test[y_test < 10**5] = 10**5
-
-
-# # Apply log10 to all the datasets
-# y_train = np.log10(y_train)
-# y_test = np.log10(y_test)
-
-
-
-
-# train_dataset = StoreDataset(X_train, y_train)
-# test_dataset = StoreDataset(X_test, y_test)
-
-# train_loader = DataLoader(train_dataset, batch_size=100, shuffle=True)
-# test_loader = DataLoader(test_dataset, batch_size=len(test_dataset), shuffle=False)
-
-# # Initialize model, loss function, and optimizer
-# in_dim = X_train.shape[1]
-# hidden_dim = 64
-
-
-# model = ElectronDensityModel(in_dim, hidden_dim).to(device)
-# criterion = nn.MSELoss()
-# optimizer = optim.Adam(model.parameters(), lr=0.001)
-
-# # Example training loop
-# num_epochs = 1  # You can adjust the number of epochs
-
-# for epoch in range(num_epochs):
-#     model.train()
-#     for inputs, targets in train_loader:
-#         inputs, targets = inputs.to(device), targets.to(device)
-        
-#         # Forward pass (assuming z is provided in the data or externally)
-#         z = torch.linspace(90, 400, 27).to(device)  # Example altitude values for electron density
-        
-    
-#         outputs = model(inputs, z)
-#         # outputs = torch.clamp(outputs, min=1e-8)
-#         # Compute the loss
-#         loss = criterion(torch.log10(outputs + 1e-8), targets)
-        
-#         # Backward pass and optimization
-#         optimizer.zero_grad()
-#         loss.backward()
-#         optimizer.step()
-    
-#     print(f'Epoch [{epoch+1}/{num_epochs}], Loss: {loss.item():.4f}')
-
-# # You can similarly implement evaluation or test loop
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# You can similarly implement evaluation or test loop
-
-
-# import torch
-# import torch.nn as nn
-# import torch.optim as optim
-# from torch.utils.data import Dataset, DataLoader
-
-# import numpy as np
-# import matplotlib.pyplot as plt
-
-# # from sklearn.datasets import make_blobs
-# from sklearn.model_selection import train_test_split
-# # from sklearn.preprocessing import StandardScaler
-
-# # Check if GPU is available
-# device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-# print(device)
-
-
-
-
-
-# # class to store data
-# class StoreDataset(Dataset):
-    
-#     def __init__(self, data, targets):
-#         self.data = torch.tensor(data, dtype=torch.float32)
-#         self.targets = torch.tensor(targets, dtype=torch.float32)
-        
-#     def __len__(self):
-#         return len(self.data)
-        
-#     def __getitem__(self, idx):
-#         return self.data[idx], self.targets[idx]
-
-
-
-# class MLP(nn.Module):
-#     def __init__(self, in_dim, hidden_dim, out_dim=8):
-#         super(MLP, self).__init__()
-        
-#         # Layers
-#         self.FC1  = nn.Linear(in_dim, hidden_dim)
-#         self.FC2  = nn.Linear(hidden_dim, out_dim)
-        
-#         # Activation
-#         self.relu = nn.ReLU()
-        
-#     def forward(self, x):
-#         x = self.FC1(x)
-#         x = self.relu(x)
-#         x = self.FC2(x)
-#         x = self.relu(x)
-#         return x
     
 
 
 
 
-
-# data_sp19 = np.load('sp19_data.npy')
-# data_eiscat = np.load('eiscat_data.npy')
-
-
-# X_train, X_test, y_train, y_test = train_test_split(data_sp19, data_eiscat, train_size=0.8, shuffle=False)
-
-
-# train_dataset = StoreDataset(X_train, y_train)
-# test_dataset = StoreDataset(X_test, y_test)
+data_sp19 = np.load('sp19_data.npy')
+data_eiscat = np.load('eiscat_data.npy')
 
 
 
 
-# # Create DataLoader
-# train_loader = DataLoader(train_dataset, batch_size=100, shuffle=True)
-# test_loader = DataLoader(test_dataset, batch_size=len(test_dataset), shuffle=False)
+X_train, X_test, y_train, y_test = train_test_split(data_sp19, data_eiscat, train_size=0.8, shuffle=True)
+
+y_train[y_train < 10**5] = 10**5
+y_test[y_test < 10**5] = 10**5
+
+
+# Apply log10 to all the datasets
+y_train = np.log10(y_train)
+y_test = np.log10(y_test)
 
 
 
@@ -251,158 +136,113 @@ print("Output:", output)
 
 
 
+train_dataset = StoreDataset(X_train, y_train)
+test_dataset = StoreDataset(X_test, y_test)
+
+train_loader = DataLoader(train_dataset, batch_size=100, shuffle=False)
+test_loader = DataLoader(test_dataset, batch_size=30, shuffle=True)
+
+# Initialize model, loss function, and optimizer
+in_dim = X_train.shape[1]
+hidden_dim = 124
+
+
+model = MLP(in_dim, out_dim=27).to(device)
+criterion = nn.MSELoss()
+optimizer = optim.Adam(model.parameters(), lr=0.001)
 
 
 
 
 
 
+# Example training loop
+num_epochs = 2000  # You can adjust the number of epochs
 
-
-# # class to store data
-# class BlobDataset(Dataset):
-    
-#     def __init__(self, data, targets):
+for epoch in range(num_epochs):
+    model.train()
+    total_loss = 0.0
+    # i=0
+    for inputs, targets in train_loader:
+        inputs, targets = inputs.to(device), targets.to(device)
         
-#         # Converting to tensors
-#         self.data = torch.tensor(data, dtype=torch.float32)
-#         self.targets = torch.tensor(targets, dtype=torch.long)
+        z = torch.linspace(90, 400, 27).to(device)
         
-#     def __len__(self):
-#         return len(self.data)
+        # print(i)
+        outputs = model(inputs, z)
+        # i+=1
+        # outputs = torch.log10(outputs)
+        # print(outputs)
+        loss = criterion(outputs, targets)
         
-#     def __getitem__(self, idx):
-#         return self.data[idx], self.targets[idx]
-
-
-
-# def plot_decision_boundary(model, X, y, device):
-#     # Set the model to evaluation mode
-#     model.eval()
-
-#     # Define the grid over which to evaluate the model
-#     x_min, x_max = X[:, 0].min() - 1, X[:, 0].max() + 1
-#     y_min, y_max = X[:, 1].min() - 1, X[:, 1].max() + 1
-#     xx, yy = np.meshgrid(np.arange(x_min, x_max, 0.01),
-#                          np.arange(y_min, y_max, 0.01))
-
-#     # Flatten the grid to pass it through the model
-#     grid = np.c_[xx.ravel(), yy.ravel()]
-#     grid_tensor = torch.tensor(grid, dtype=torch.float32).to(device)
-
-#     # Get model predictions for the grid
-#     with torch.no_grad():
-#         Z = model(grid_tensor)
-#         Z = torch.argmax(Z, dim=1)
-#         Z = Z.cpu().numpy()
-
-#     # Reshape the predictions back into the grid shape
-#     Z = Z.reshape(xx.shape)
-
-#     # Plot the decision boundary and the data points
-#     plt.contourf(xx, yy, Z, alpha=0.5, cmap=plt.cm.RdYlBu)
-#     plt.scatter(X[:, 0], X[:, 1], c=y, s=40, edgecolor='k', cmap=plt.cm.RdYlBu)
-#     plt.xlim(xx.min(), xx.max())
-#     plt.ylim(yy.min(), yy.max())
-#     plt.title("Decision Boundary")
-#     plt.show()
-
-
-
-
-
-
-# # data
-# X, y = make_blobs(n_samples=1000, centers=2, n_features=2)
-
-
-# # Standardize
-# scaler = StandardScaler()
-# X = scaler.fit_transform(X)
-
-
-# # Split
-# X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, stratify=y, random_state=1)
-
-
-# # plot
-# plt.figure()
-# plt.title("Blobs dataset")
-# plt.scatter(X_train[:,0][y_train==0], X_train[:,1][y_train==0], color="C0", label="Class 1")
-# plt.scatter(X_train[:,0][y_train==1], X_train[:,1][y_train==1], color="C1", label="Class 2")
-# plt.xlabel("X1")
-# plt.ylabel("X2")
-# plt.legend()
-# plt.show()
-
-
-
-
-# # Storing datasets
-# train_dataset = BlobDataset(X_train, y_train)
-# test_dataset = BlobDataset(X_test, y_test)
-
-
-# # Create DataLoader
-# train_loader = DataLoader(train_dataset, batch_size=100, shuffle=True)
-# test_loader = DataLoader(test_dataset, batch_size=len(test_dataset), shuffle=False)
-
-
-
-
-# # Initialize network
-# model = MLP(2, 10, 2).to(device)
-# criterion = nn.CrossEntropyLoss()
-# optimizer = optim.Adam(model.parameters(), lr=0.01)
-
-
-
-# epochs = 100
-
-# for epoch in range(epochs):
-#     model.train()
-#     running_loss = 0.0
-#     for inputs, labels in train_loader:
         
-#         inputs, labels = inputs.to(device), labels.to(device)
+        # Backward pass and optimization
+        optimizer.zero_grad()
+        loss.backward()
         
-#         optimizer.zero_grad()
-#         outputs = model(inputs)
-#         loss = criterion(outputs, labels)
-#         loss.backward()
-#         optimizer.step()
-#         running_loss += loss.item()
-    
-    
-#     print(f'Epoch [{epoch+1}/{epochs}], Loss: {running_loss/len(train_loader):.4f}')
-
-
-
-# # Test the model
-# model.eval()
-# correct = 0
-# total = 0
-
-
-
-
-# with torch.no_grad():
-#     for inputs, labels in test_loader:
-#         # Move inputs and labels to the device
-#         inputs, labels = inputs.to(device), labels.to(device)
+        optimizer.step()
         
-#         outputs = model(inputs)
-#         _, predicted = torch.max(outputs.data, 1)
-#         total += labels.size(0)
-#         correct += (predicted == labels).sum().item()
-
-# print(f'Accuracy of the model on the test set: {100 * correct / total:.2f}%')
-
+        total_loss += loss.item()
+        
+    # print(outputs)
+    avg_train_loss = total_loss / len(train_loader)
+    print(f'Epoch [{epoch+1}/{num_epochs}], Loss: {avg_train_loss:.4f}')
 
 
 
 
+# Set the model to evaluation mode
+model.eval()
 
-# plot_decision_boundary(model, X, y, device)
+# Disable gradient computation for evaluation
+with torch.no_grad():
+    for inputs, targets in test_loader:
+        inputs, targets = inputs.to(device), targets.to(device)
+        
+        # Define the range of z values
+        z = torch.linspace(90, 400, 27).to(device)
+        
+        # Get model predictions
+        outputs = model(inputs, z)
+        
+        # Convert the outputs and targets to NumPy arrays for plotting
+        outputs_np = outputs.cpu().numpy()
+        targets_np = targets.cpu().numpy()
+        
+        print(f'Predictions for 10 test samples (numpy):\n{outputs_np}')
+        print(f'True targets for 10 test samples (numpy):\n{targets_np}')
+        break  # Exit after processing the first 10 samples
+
+# Now you can use matplotlib to plot the outputs and targets
+# Example plot:
+import matplotlib.pyplot as plt
+
+plt.figure(figsize=(10, 6))
+for i in range(len(outputs_np)):
+    plt.plot(outputs_np[i], z.cpu().numpy(), label='Predicted', color="C0")
+    plt.plot(targets_np[i], z.cpu().numpy(), label='True', color="C1")
+
+    plt.xlabel('Altitude (km)')
+    plt.ylabel('Log Electron Density')
+    # plt.title('Predicted vs True Electron Density for 10 Samples')
+    plt.legend()
+    # plt.grid(True)
+    plt.show()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
