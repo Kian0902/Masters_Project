63c493020302e96fa8d65b66fe9851be8cc715bd
Kian Sartipzadeh <kian0902@hotmail.com>
2024-09-23 10:59:43 +0200

    Clean up.

--- AI/Deep_Learning/MLP/main_mlp.py
+++ AI/Deep_Learning/MLP/main_mlp.py
@@ -40,12 +40,10 @@ class MLP(nn.Module):
         super(MLP, self).__init__()
         
         # MLP layers
-        self.FC1  = nn.Linear(in_dim, 128)
-        self.FC2  = nn.Linear(128, 128)
-        self.FC3  = nn.Linear(128, 64)
-        self.FC4  = nn.Linear(64, out_dim)
-        
-        
+        self.FC1  = nn.Linear(in_dim, 64)
+        self.FC2  = nn.Linear(64, out_dim)
+
+ 
         # Activation
         self.relu = nn.ReLU()
         self.softplus = nn.Softplus(beta=0.2, threshold=1)
@@ -53,18 +51,6 @@ class MLP(nn.Module):
     
     def double_chapman(self, x, z):
         zE_peak, zF_peak, nE_peak, nF_peak, HE_below, HF_below, HE_above, HF_above = x.split(1, dim=1)
-        
-        # print(f"zE_peak:  min={torch.min(zE_peak)}, max={torch.max(zE_peak)}")
-        # print(f"zF_peak:  min={torch.min(zF_peak)}, max={torch.max(zF_peak)}")
-        # print(f"nE_peak:  min={torch.min(nE_peak)}, max={torch.max(nE_peak)}")
-        # print(f"nF_peak:  min={torch.min(nF_peak)}, max={torch.max(nF_peak)}")
-        # print(f"HE_below:  min={torch.min(HE_below)}, max={torch.max(HE_below)}")
-        # print(f"HF_below:  min={torch.min(HF_below)}, max={torch.max(HF_below)}")
-        # print(f"nE_peak:  min={torch.min(HE_above)}, max={torch.max(HE_above)}")
-        # print(f"nF_peak:  min={torch.min(HF_above)}, max={torch.max(HF_above)}")
-        # print("\n")
-        # print(zE_peak.shape)
-        # print(torch.min(zE_peak))
         
         
         # Adding epsilon to avoid division by zero
@@ -74,12 +60,8 @@ class MLP(nn.Module):
         # Clamping to avoid overflow in torch.exp
         neE = nE_peak * torch.exp(1 - ((z - zE_peak) / HE) - torch.exp(-((z - zE_peak) / HE)))
         neF = nF_peak * torch.exp(1 - ((z - zF_peak) / HF) - torch.exp(-((z - zF_peak) / HF)))
-        # Calculate min and max for neE and neF
-        neE_min, neE_max = torch.min(neE), torch.max(neE)
-        neF_min, neF_max = torch.min(neF), torch.max(neF)
-        
-        # print(neE_min, neE_max)
-        # print(neF_min, neF_max)
+
+
         return neE + neF
         
     
@@ -89,23 +71,12 @@ class MLP(nn.Module):
         x = self.FC1(x)
         x = self.relu(x)
         x = self.FC2(x)
-        x = self.relu(x)
-        x = self.FC3(x)
-        x = self.relu(x)
-        x = self.FC4(x)
         x = self.softplus(x)
         
-        # Clamp parameters within specified ranges
-        # min_values = torch.tensor([80, 200, 3, 3, 3, 3, 3, 3], device=x.device)  # Set appropriate lower bounds for each parameter
-        # max_values = torch.tensor([200, 400, 15, 15, 100, 100, 100, 100], device=x.device)  # Set appropriate upper bounds for each parameter
-    
-        # x = torch.clamp(x, min=min_values, max=max_values)
-            
-        batch_siz1e = x.size(0)
-        # z = z.unsqueeze(0).expand(batch_size, -1).to(device)
+        batch_size = x.size(0)
+        z = z.unsqueeze(0).expand(batch_size, -1).to(device)
         
-        # chapman_output = self.double_chapman(x, z)
-        # print(torch.min(chapman_output, 0))
+        chapman_output = self.double_chapman(x, z)
         
         return x
     
@@ -140,7 +111,7 @@ train_dataset = StoreDataset(X_train, y_train)
 test_dataset = StoreDataset(X_test, y_test)
 
 train_loader = DataLoader(train_dataset, batch_size=100, shuffle=False)
-test_loader = DataLoader(test_dataset, batch_size=30, shuffle=True)
+test_loader = DataLoader(test_dataset, batch_size=len(test_dataset), shuffle=True)
 
 # Initialize model, loss function, and optimizer
 in_dim = X_train.shape[1]
@@ -162,17 +133,12 @@ num_epochs = 2000  # You can adjust the number of epochs
 for epoch in range(num_epochs):
     model.train()
     total_loss = 0.0
-    # i=0
     for inputs, targets in train_loader:
         inputs, targets = inputs.to(device), targets.to(device)
         
         z = torch.linspace(90, 400, 27).to(device)
         
-        # print(i)
         outputs = model(inputs, z)
-        # i+=1
-        # outputs = torch.log10(outputs)
-        # print(outputs)
         loss = criterion(outputs, targets)
         
         
