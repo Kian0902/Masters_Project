a053a80610488c6dc04015a09fc1a0840c9d1b56
Kian Sartipzadeh <kian0902@hotmail.com>
2024-09-23 13:26:51 +0200

    Added code for calculating the sensitivity stability of 8 DChap parameters per altitude.

--- AI/Feature_Analysis/Numerical_Stability/main_stability.py
+++ AI/Feature_Analysis/Numerical_Stability/main_stability.py
@@ -7,15 +7,18 @@ Created on Mon Sep 23 11:45:00 2024
 
 
 
+import numpy as np
+import matplotlib.pyplot as plt
+
 from SALib.sample import saltelli
 from SALib.analyze import sobol
-import numpy as np
 
 
 
 
 
-def double_chapman(self, z, HEd, HEu, HFd, HFu, zE_peak, zF_peak, neE_peak, neF_peak):
+
+def double_chapman(z, HEd, HEu, HFd, HFu, zE_peak, zF_peak, neE_peak, neF_peak):
     """
     Function for the double chapman electron density model.
     
@@ -50,29 +53,80 @@ def double_chapman(self, z, HEd, HEu, HFd, HFu, zE_peak, zF_peak, neE_peak, neF_
 
 
 
+def eval_model(z, parm):
+    HEd, HEu, HFd, HFu, neE_peak, neF_peak, zE_peak, zF_peak = parm
+    return double_chapman(z, HEd, HEu, HFd, HFu, zE_peak, zF_peak, neE_peak, neF_peak)
+
+
+
 problem = {'num_vars': 8,
-           'names': ['HEd',
-                     'HEu',
-                     'HFd', 
-                     'HFu',
-                     'log_neE_peak', 
-                     'log_neF_peak',
-                     'zE_peak',
-                     'zF_peak'],
+           'names': ['HEd','HEu','HFd', 'HFu',
+                     'log_neE_peak', 'log_neF_peak',
+                     'zE_peak','zF_peak'],
            
-           'bounds': [[1, 100],
-                      [1, 100],
-                      [1, 100],
-                      [1, 100],
-                      [5, 16],
-                      [5, 16],
-                      [80, 600],
-                      [80, 600]]
+           'bounds': [[1, 100], [1, 100], [1, 100], [1, 100],
+                      [5, 16], [5, 16],
+                      [80, 600], [80, 600]]
            }
 
 
 
 
+
+# generate parameter samples
+param_values = saltelli.sample(problem, 2**8)
+
+
+z = np.linspace(90, 400, 27)
+
+
+
+# Evaluate the model for each sample
+Y = np.array([eval_model(z, param) for param in param_values])
+
+
+
+Si = []
+for i in range(len(z)):
+    a = sobol.analyze(problem, Y[:, i])
+    Si.append(a)
+
+
+
+
+# Print the sensitivity indices for each altitude
+for i, altitude in enumerate(z):
+    print(f"Altitude: {altitude} km")
+    print("Total-order indices: ", Si[i]['ST'])
+    print()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# A = double_chapman(z, 10, 15, 20, 50, 150, 270, 1e11, 1e12)
+# A[A < 1] = 1e5
+
+
+# A = np.log10(A)
+
+# B = np.round(A, decimals=3)
+
+
+
+
+# plt.plot(A, z)
+# plt.plot(B, z)
+# plt.show()
 
 
 
