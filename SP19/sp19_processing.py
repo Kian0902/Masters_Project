29e23ffcbc93ff11d5ba3896247f911fece0d35e
Kian Sartipzadeh <kian0902@hotmail.com>
2024-09-24 15:16:56 +0200

    m¨

--- SP19/sp19_processing.py
+++ SP19/sp19_processing.py
@@ -5,21 +5,60 @@ Created on Tue Sep 24 13:32:17 2024
 @author: Kian Sartipzadeh
 """
 
-
-
+custom_header=['DoY/366', 'ToD/1440', 'Solar_Zenith/44', 'Kp', 'R', 'Dst',
+                'ap', 'F10_7', 'AE', 'AL', 'AU', 'PC_potential', 'Lyman_alpha',
+                'Bx', 'By', 'Bz', 'dBx', 'dBy', 'dBz']
 
 
 import numpy as np
+import pandas as pd
+import seaborn as sns
 import matplotlib.pyplot as plt
+from sklearn.neighbors import LocalOutlierFactor
 
 
 
 
+class Processing:
+    
+    def __init__(self, dataset):
+        self.dataset = dataset
+    
+    
+    def filter_missing_values(self):
+        
+        if self.dataset.isnull().values.any():
+            self.dataset = self.dataset.interpolate(method='linear', axis=0, limit_direction='forward')
+        
+        else:
+            print("No missing values detected!")
+    
+    
+    
+    def plot_hist(self, feature=None):
+        
+        data = self.dataset.to_numpy()
+        
+        print(len(data))
+        
+        sns.histplot(data[:,feature], bins=int(len(data)/10), kde=True, color="C0")
+        plt.xlabel('Values')
+        plt.ylabel('Density')
+        plt.title(f'Feature {feature}')
+        plt.show()
+        
+
+data = np.load("sp19_features.npy")
 
 
+a=1000
+
+X = pd.DataFrame(data[:a])
 
 
-
+A = Processing(X)
+A.filter_missing_values()
+A.plot_hist(feature=3)
 
 
 
