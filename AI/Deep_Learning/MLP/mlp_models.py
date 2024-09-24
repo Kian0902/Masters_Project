3d963269ba7c06501456d670f2c2607512fefe92
Kian Sartipzadeh <kian0902@hotmail.com>
2024-09-24 17:38:26 +0200

    Created python file for storing mlp models.

---
+++ AI/Deep_Learning/MLP/mlp_models.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 24 17:35:25 2024
+
+@author: Kian Sartipzadeh
+"""
+
+
+
+
+import torch
+import torch.nn as nn
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
