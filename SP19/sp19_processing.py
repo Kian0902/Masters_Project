33e36076cb90d2bd2433a43de9969de2f347f2f6
Kian Sartipzadeh <kian0902@hotmail.com>
2024-09-24 13:33:39 +0200

    Created python file for processing sp19 data.

---
+++ SP19/sp19_processing.py
@@ -0,0 +1,64 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 24 13:32:17 2024
+
+@author: Kian Sartipzadeh
+"""
+
+
+
+
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
