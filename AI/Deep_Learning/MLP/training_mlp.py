99889b23f93c267eec75b2a2d1ea73a372d221b4
Kian Sartipzadeh <kian0902@hotmail.com>
2024-09-25 15:20:01 +0200

    Moved training loop to new file.

--- AI/Deep_Learning/MLP/training_mlp.py
+++ AI/Deep_Learning/MLP/training_mlp.py
@@ -2,6 +2,72 @@
 """
 Created on Wed Sep 25 14:45:11 2024
 
-@author: kian0
+@author: Kian Sartipzadeh
 """
+
+import torch
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+
+def train_model(model, train_loader, val_loader, loss_function, optimizer, scheduler, device, num_epochs, model_path):
+    best_val_loss = float('inf')
+
+    for epoch in range(num_epochs):
+        model.train()
+        total_train_loss = 0.0
+        for inputs, targets in train_loader:
+            inputs, targets = inputs.to(device), targets.to(device)
+            
+            z = torch.linspace(90, 400, 27).to(device)
+            outputs = model(inputs, z)
+            loss = loss_function(outputs, targets)
+            
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+            
+            total_train_loss += loss.item()
+        
+        avg_train_loss = total_train_loss / len(train_loader)
+        
+        # Validation loop
+        model.eval()
+        total_val_loss = 0.0
+        with torch.no_grad():
+            for val_inputs, val_targets in val_loader:
+                val_inputs, val_targets = val_inputs.to(device), val_targets.to(device)
+                
+                z = torch.linspace(90, 400, 27).to(device)
+                val_outputs = model(val_inputs, z)
+                val_loss = loss_function(val_outputs, val_targets)
+                total_val_loss += val_loss.item()
+        
+        avg_val_loss = total_val_loss / len(val_loader)
+        
+        scheduler.step()
+
+        if avg_val_loss < best_val_loss:
+            best_val_loss = avg_val_loss
+            torch.save(model.state_dict(), model_path)
+        
+        print(f'Epoch [{epoch+1}/{num_epochs}], Train Loss: {avg_train_loss:.4f}, Val Loss: {avg_val_loss:.4f}, LR: {scheduler.get_last_lr()[0]:.6f}')
+    
+    return model
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
