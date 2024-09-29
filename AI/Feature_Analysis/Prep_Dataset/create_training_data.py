b19087b85b04f2473abcc1a7ee3185c8b37e337c
Kian Sartipzadeh <kian0902@hotmail.com>
2024-09-29 16:10:52 +0200

    Committed files that were left uncommotted for some reason.

--- AI/Feature_Analysis/Prep_Dataset/create_training_data.py
+++ AI/Feature_Analysis/Prep_Dataset/create_training_data.py
@@ -7,59 +7,63 @@ Created on Tue Sep 18 14:42:19 2024
 
 
 import os
-import numpy as np
-import pandas as pd
 
 
 
 def list_csv_files(folder_path):
     return [f for f in os.listdir(folder_path) if f.endswith('.csv')]
 
+
+def list_png_files(folder_path):
+    return [f[9:] for f in os.listdir(folder_path) if f.endswith('.png')]
+
+
 def get_filename_without_extension(filename):
     return os.path.splitext(filename)[0]
 
 
 radar_names = list_csv_files("EISCAT_samples")
-satellite_names = list_csv_files("SP19_samples")
+ionogram_names = list_png_files("Ionogram_sampled_images")
 
 
 # Extract base filenames without ".csv"
 radar_filenames = set(get_filename_without_extension(f) for f in radar_names)
-satellite_filenames = set(get_filename_without_extension(f) for f in satellite_names)
+ionogram_filenames = set(get_filename_without_extension(f) for f in ionogram_names)
+
 
 # Find the matching filenames
-matching_filenames = sorted(list(satellite_filenames.intersection(radar_filenames)))
+matching_filenames = sorted(list(ionogram_filenames.intersection(radar_filenames)))
 
 
 
 
 
-radar_data_list = []
-satellite_data_list = []
+# radar_data_list = []
+# satellite_data_list = []
 
-for filename in matching_filenames:
-    radar_file_path = os.path.join("EISCAT_samples", filename + ".csv")
-    satellite_file_path = os.path.join("SP19_samples", filename + ".csv")
+# for filename in matching_filenames:
+#     radar_file_path = os.path.join("EISCAT_samples", filename + ".csv")
+#     satellite_file_path = os.path.join("SP19_Ionogram_samples", filename + ".csv")
     
     
-    radar_data = np.loadtxt(radar_file_path, delimiter=',')
-    satellite_data = np.loadtxt(satellite_file_path, delimiter=',')
+#     radar_data = np.loadtxt(radar_file_path, delimiter=',')
+#     satellite_data = np.loadtxt(satellite_file_path, delimiter=',')
     
     
-    radar_data_list.append(radar_data)
-    satellite_data_list.append(satellite_data)
+#     radar_data_list.append(radar_data)
+#     satellite_data_list.append(satellite_data)
 
 
-radar_data = np.array(radar_data_list)
-satellite_data= np.array(satellite_data_list)
+# radar_data = np.array(radar_data_list)
+# satellite_data= np.array(satellite_data_list)
 
 
-print(radar_data[0])
-print(satellite_data[0])
+# print(radar_data[0])
+# print(satellite_data[0])
 
 
-np.save('eiscat_data.npy', radar_data)
-np.save('sp19_data.npy', satellite_data)
+# np.save('eiscat_data_SI.npy', radar_data)
+# np.save('SP19_Ionogram_data.npy', satellite_data)
 
 
 
--- AI/Feature_Analysis/Prep_Dataset/sorting_files_sp19_ionogram.py
+++ AI/Feature_Analysis/Prep_Dataset/sorting_files_sp19_ionogram.py
@@ -2,6 +2,114 @@
 """
 Created on Thu Sep 26 16:30:46 2024
 
-@author: kian0
+@author: Kian Sartipzadeh
 """
+
+
+
+
+import os
+import glob
+import pandas as pd
+
+
+
+# custom_header=['DoY/366', 'ToD/1440', 'Solar_Zenith/44', 'Kp', 'R', 'Dst',
+#                'ap', 'F10_7', 'AE', 'AL', 'AU', 'PC_potential', 'Lyman_alpha',
+#                'Bx', 'By', 'Bz', 'dBx', 'dBy', 'dBz']
+
+
+folder_path = "ionograms_1D"
+
+
+df_list = []
+
+
+
+for i, filename in enumerate(os.listdir(folder_path)):
+    if filename.endswith('.csv'):
+        if i % 1000 == 0:
+            print(f'{i}/{len(os.listdir(folder_path))}')
+        
+        file_path = os.path.join(folder_path, filename)
+        
+        
+        df = pd.read_csv(file_path, header=None, skiprows=1)
+        
+        df = df.T
+        
+        
+        df.to_csv(f'SP19_Ionogram_samples/{filename}', index=False, header=False)
+        
+        
+        
+        
+        # df_list.append(df)
+
+# combined_df = pd.concat(df_list, axis=0)
+# combined_df.to_csv('spacephysics_19features.csv', index=False, header=custom_header)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
