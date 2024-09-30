79a668019658c7820f56009cb6773044eb86ecf3
Kian Sartipzadeh <kian0902@hotmail.com>
2024-09-30 15:29:12 +0200

    Made code compatible with all other changes.

--- AI/Deep_Learning/CNN/main_cnn.py
+++ AI/Deep_Learning/CNN/main_cnn.py
@@ -16,78 +16,42 @@ from sklearn.model_selection import train_test_split
 
 
 from utils import plot_ionogram
-from storing_dataset import IonoEisDataset
+from storing_dataset import StoreDataset, MatchingPairs
 from cnn_models import CNN
 
-
-
-
-def list_csv_files(folder_path):
-    return [f for f in os.listdir(folder_path) if f.endswith('.csv')]
-
-
-def list_png_files(folder_path):
-    return [f for f in os.listdir(folder_path) if f.endswith('.png')]
-
-
-def get_filename_without_extension(filename):
-    return os.path.splitext(filename)[0]
-
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+print(device)
 
 
 
 radar_folder = "EISCAT_samples"
 ionogram_folder = "Ionogram_sampled_images"
 
-radar_names = list_csv_files(radar_folder)
-ionogram_names = list_png_files(ionogram_folder)
-
-# Extract base filenames without ".csv"
-radar_filenames = set(get_filename_without_extension(f) for f in radar_names)
-ionogram_filenames = set(get_filename_without_extension(f) for f in ionogram_names)
-
-# Find the matching filenames
-matching_filenames = sorted(list(ionogram_filenames.intersection(radar_filenames)))
-transform = transforms.Compose([transforms.ToTensor()])
-A = IonoEisDataset(ionogram_folder, radar_folder, matching_filenames, transform=transform)
 
 
+Pairs = MatchingPairs(ionogram_folder, radar_folder)
 
-sample = DataLoader(A, batch_size=2, shuffle=False)
+rad, ion = Pairs.find_pairs()
+
+
+A = StoreDataset(ion, rad, transforms.Compose([transforms.ToTensor()]))
+
+
+data_loader = DataLoader(A, batch_size=100, shuffle=True)
 
 
 model = CNN()
-
-# Iterate through the DataLoader to get images
-for image, _ in sample:
-    print(image.shape)  # Ensure this prints [1, 3, 81, 81]
-    y = model(image)
-    
-    for i in range(5):
-        print(y[i].shape)
-        
-    break
+criterion = nn.MSELoss()  # Assuming this is a classification problem
+optimizer = optim.Adam(model.parameters(), lr=0.1)  # Use Adam optimizer
 
 
 
+# Function to count the number of parameters
+def count_parameters(model):
+    return sum(p.numel() for p in model.parameters() if p.requires_grad)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+# Print the number of parameters
+print(f"Total number of trainable parameters: {count_parameters(model)}")
 
 
 
