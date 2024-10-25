27cc5d19f665cf0ccdca5bc6de16cc53eb1446d0
Kian0902 <kian0902@hotmail.com>
2024-10-25 17:51:22 +0200

    Final commit for this code.

--- misc/Supervised_VS_Unsupervised.py
+++ misc/Supervised_VS_Unsupervised.py
@@ -18,16 +18,19 @@ X, y, centers = make_blobs(n_samples=100, centers=2, n_features=2, random_state=
 
 # Example Figure Supervised
 fig, ax = plt.subplots(figsize=(6, 5))
+fig.suptitle("Supervised", fontsize=20)
 ax.scatter(X[y == 0][:, 0], X[y == 0][:, 1], color="C0", edgecolor='k', marker='o', s=100, label="Class 0")
 ax.scatter(X[y == 1][:, 0], X[y == 1][:, 1], color="C1", edgecolor='k', marker='o', s=100, label="Class 1")
 ax.spines[['right', 'top']].set_visible(False)
 ax.spines[['left' , 'bottom']].set_linewidth(2)
 ax.tick_params(left=False, labelleft=False, bottom=False, labelbottom=False)
+plt.savefig("Example_Supervised.png", dpi=150, bbox_inches='tight')
 plt.show()
 
 
 # Example Figure Unsupervised
 fig, ax = plt.subplots(figsize=(6, 5))
+fig.suptitle("Unsupervised", fontsize=20)
 ax.scatter(X[:, 0], X[:, 1], color="grey", edgecolor='k', marker='o', s=100)
 circle1 = plt.Circle((centers[0][0],centers[0][1]), radius=4, linewidth=2, linestyle="--", fill=False)
 circle2 = plt.Circle((centers[1][0],centers[1][1]+1), radius=4, linewidth=2, linestyle="--", fill=False)
@@ -36,6 +39,7 @@ ax.add_artist(circle2)
 ax.spines[['right', 'top']].set_visible(False)
 ax.spines[['left' , 'bottom']].set_linewidth(2)
 ax.tick_params(left=False, labelleft=False, bottom=False, labelbottom=False)
+plt.savefig("Example_Unsupervised.png", dpi=150, bbox_inches='tight')
 plt.show()
 
 
