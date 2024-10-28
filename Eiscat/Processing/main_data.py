fb3770ade87d35ba5a2deba28979b73648dca10c
Kian0902 <kian0902@hotmail.com>
2024-10-28 16:09:38 +0100

    Processed hdf5 files from madrigal.

--- Eiscat/Processing/main_data.py
+++ Eiscat/Processing/main_data.py
@@ -15,74 +15,78 @@ from read_EISCAT_data import EISCATDataProcessor
 from data_sorting import EISCATDataSorter
 from data_averaging import EISCATAverager
 from data_filtering import EISCATDataFilter
-from data_outlier_detection import EISCATOutlierDetection
+# from data_outlier_detection import EISCATOutlierDetection
 
 
 
 
-def save_data(dataset: dict, file_name: str):
-    with open(file_name, 'wb') as file:
-        pickle.dump(dataset, file)
+# def save_data(dataset: dict, file_name: str):
+#     with open(file_name, 'wb') as file:
+#         pickle.dump(dataset, file)
 
 
 
-def detect_nan_in_arrays(data_dict):
-    for key, array in data_dict.items():
-        if np.isnan(array).any():
-            print(f"NaN detected {key}: {array.shape}")
+# def detect_nan_in_arrays(data_dict):
+#     for key, array in data_dict.items():
+#         if np.isnan(array).any():
+#             print(f"NaN detected {key}: {array.shape}")
 
 
 # Use the local folder name containing data
-folder_name = "Processing_outputs\\Ne_uhf_madrigal"
+folder_name_in  = "EISCAT_Madrigal"
+folder_name_out = "EISCAT_MAT"
 
 
-# Sorting data
-Esicat = EISCATDataSorter(folder_name)
-Esicat.sort_data()  # sort data
-
-
-# UHF data
-X_Eiscat = Esicat.return_data()  # returning dict data
+# Extract info from hdf5 files
+madrigal_processor = EISCATDataProcessor(folder_name_in, folder_name_out)
+madrigal_processor.process_all_files()
 
 
 
-
-# Clipping range and Filtering data for nan
-filt = EISCATDataFilter(X_Eiscat, filt_range=True, filt_nan=True) 
-filt.batch_filtering()
-X_filtered = filt.return_data()
-
-
-for key in X_filtered:
-    detect_nan_in_arrays(X_filtered[key])
-
-
-
-# Detecting outliers
-Outlier = EISCATOutlierDetection(X_filtered)
-Outlier.batch_detection(method_name="IQR", save_plot=False)
-X_outliers = Outlier.return_outliers()
-
-
-# Filtering outliers
-outlier_filter = EISCATDataFilter(X_filtered, filt_outlier=True)
-outlier_filter.batch_filtering(dataset_outliers=X_outliers, filter_size=3, plot_after_each_day=False)
-X_outliers_filtered = outlier_filter.return_data()
+# # Sorting data
+# Eiscat = EISCATDataSorter(folder_name)
+# Eiscat.sort_data()               # sort data
+# X_Eiscat = Eiscat.return_data()  # returning dict data
 
 
 
 
-
-# Averaging data
-AVG = EISCATAverager(X_outliers_filtered)
-AVG.batch_averaging(save_plot=False, weighted=False)
-X_avg = AVG.return_data()
-
-
-# # print(X_avg['2021-3-10']['r_time'])
+# # Clipping range and Filtering data for nan
+# filt = EISCATDataFilter(X_Eiscat, filt_range=True, filt_nan=True) 
+# filt.batch_filtering()
+# X_filtered = filt.return_data()
 
 
-save_data(X_avg, file_name="Ne_uhf_avg")
+# for key in X_filtered:
+#     detect_nan_in_arrays(X_filtered[key])
+
+
+
+# # Detecting outliers
+# Outlier = EISCATOutlierDetection(X_filtered)
+# Outlier.batch_detection(method_name="IQR", save_plot=False)
+# X_outliers = Outlier.return_outliers()
+
+
+# # Filtering outliers
+# outlier_filter = EISCATDataFilter(X_filtered, filt_outlier=True)
+# outlier_filter.batch_filtering(dataset_outliers=X_outliers, filter_size=3, plot_after_each_day=False)
+# X_outliers_filtered = outlier_filter.return_data()
+
+
+
+
+
+# # Averaging data
+# AVG = EISCATAverager(X_outliers_filtered)
+# AVG.batch_averaging(save_plot=False, weighted=False)
+# X_avg = AVG.return_data()
+
+
+# # # print(X_avg['2021-3-10']['r_time'])
+
+
+# save_data(X_avg, file_name="Ne_uhf_avg")
 
 
 
