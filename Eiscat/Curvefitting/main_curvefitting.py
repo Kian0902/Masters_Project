8b9b227a16167e9e3cfe08c2891b078a93333b26
Kian0902 <kian0902@hotmail.com>
2024-10-31 15:42:01 +0100

    minor changes

--- Eiscat/Curvefitting/main_curvefitting.py
+++ Eiscat/Curvefitting/main_curvefitting.py
@@ -14,28 +14,28 @@ from curvefitting_evaluation import CurvefittingEvaluation
 
 
 # Importing processed dataset
-custom_file_path = "Curvefitting_inputs/Ne_uhf_avg"
+custom_file_path = "X_avg"
 with open(custom_file_path, 'rb') as f:
     dataset = pickle.load(f)
 
 
 
 
-key_choise = list(dataset.keys())[:]
+key_choise = list(dataset.keys())[:3]
 X = {key: dataset[key] for key in key_choise}
 
 
 
 
-# m = 'scipy'
-m = 'lmfit'
-# m = 'NN'
+# # # m = 'scipy'
+# m = 'lmfit'
+# # # m = 'NN'
 
-A = CurvefittingChapman(X)
-A.batch_detection(model_name=m, H_initial=[5, 10, 25, 40], save_plot=False)
-A.save_curvefits(custom_file_path + "_" + m + "_curvefits")
+# A = CurvefittingChapman(X)
+# A.batch_detection(model_name=m, H_initial=[5, 10, 25, 40], save_plot=False)
+# A.save_curvefits(custom_file_path + "_" + m + "_curvefits")
 
-x = A.return_curvefits()
+# x = A.return_curvefits()
 
 
 def import_file(file_name):
@@ -45,17 +45,15 @@ def import_file(file_name):
     return dataset
 
 
-
-
-# Import files
-file_org = "Curvefitting_inputs/Ne_uhf_avg"
-file_fit = "Curvefitting_outputs/Ne_uhf_avg_lmfit_curvefits"
+# # Import files
+file_org = "X_avg"
+file_fit = "X_avg_lmfit_curvefits"
 
 # Get datasets
 X_org = import_file(file_org)
 X_fit = import_file(file_fit)
 
-key_choise = list(X_org.keys())[:]
+key_choise = list(X_fit.keys())[:]
 
 
 x_org = {key: X_org[key] for key in key_choise}
@@ -64,64 +62,9 @@ x_fit = {key: X_fit[key] for key in key_choise}
 
 
 E = CurvefittingEvaluation(x_org, x_fit)
-E.batch_detection(eval_method="Normalized Residuals", show_plot=True, save_plot=True)
+E.batch_detection(eval_method="Normalized Residuals", show_plot=True, save_plot=False)
 # E.residual_norm(x_org, x_fit, show_plot=True, save_plot=True)
 # E.chi_square(x_org, x_fit, show_plot=True)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
