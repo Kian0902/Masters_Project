51f61f864ff6bfa15f67a779d0052200a1a67811
Kian0902 <kian0902@hotmail.com>
2024-11-08 12:28:52 +0100

    Added average pooling example

--- misc/Max_Pooling.py
+++ misc/Max_Pooling.py
@@ -17,7 +17,6 @@ def load_image(image_path):
     image = cv2.imread(image_path)
     image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
     # Resize image to a square for simplicity
-    image = cv2.resize(image, (128, 128))
     # Normalize image to [0, 1]
     image = image / 255.0
     # Convert image to PyTorch tensor and add a batch dimension
@@ -28,22 +27,36 @@ def load_image(image_path):
 def plot_image(image_tensor, title):
     # Remove batch dimension and permute to H x W x C for plotting
     image = image_tensor.squeeze().permute(1, 2, 0).detach().numpy()
+    height, width = image.shape[:2]
     plt.imshow(image)
-    plt.title(title)
-    plt.axis('off')
+    plt.title(f"{title}")
+    plt.xlabel(f'{width} pixels', fontsize=13)
+    plt.ylabel(f'{height} pixels', fontsize=13)
+    plt.tick_params(left=False, right=False , labelleft=False, 
+                labelbottom=False,bottom = False) 
     plt.show()
 
 # Define maxpooling operation layers
 maxpool_layers = [
-    nn.MaxPool2d(kernel_size=2, stride=2),
-    nn.MaxPool2d(kernel_size=2, stride=2),
-    nn.MaxPool2d(kernel_size=2, stride=2)
+    nn.MaxPool2d(kernel_size=2),
+    nn.MaxPool2d(kernel_size=2),
+    nn.MaxPool2d(kernel_size=2),
+    nn.MaxPool2d(kernel_size=2)
 ]
+
+# Define maxpooling operation layers
+avgpool_layers = [
+    nn.AvgPool2d(kernel_size=2),
+    nn.AvgPool2d(kernel_size=2),
+    nn.AvgPool2d(kernel_size=2),
+    nn.AvgPool2d(kernel_size=2)
+]
+
 
 # Main function
 def main():
     # Load your image (provide the correct path to your image)
-    image_path = 'your_image_path_here.jpg'
+    image_path = 'Iono_ex_color.png'
     image_tensor = load_image(image_path)
 
     # Plot the original image
@@ -51,9 +64,11 @@ def main():
 
     # Apply maxpooling layers and plot after each layer
     current_tensor = image_tensor
-    for i, maxpool in enumerate(maxpool_layers):
-        current_tensor = maxpool(current_tensor)
-        plot_image(current_tensor, f'After MaxPool Layer {i+1}')
+    for i, avgpool in enumerate(avgpool_layers):
+        current_tensor = avgpool(current_tensor)
+        plot_image(current_tensor, f'AvgPool Layer {i+1}')
+
+
 
 if __name__ == "__main__":
     main()
