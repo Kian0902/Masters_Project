f0f75f277e237c5b38a7361b640ddc4772b684ae
Kian0902 <kian0902@hotmail.com>
2024-11-27 16:13:49 +0100

    Imported utils

--- Artist/main_artist.py
+++ Artist/main_artist.py
@@ -5,18 +5,18 @@ Created on Tue Nov 26 15:11:24 2024
 @author: Kian Sartipzadeh
 """
 
-
+import os
 import pickle
-import numpy as np
-import matplotlib.pyplot as plt
+# import numpy as np
+import pandas as pd
+# import matplotlib.pyplot as plt
 
-from datetime import datetime
-from matplotlib.dates import DateFormatter
+# from datetime import datetime
+# from matplotlib.dates import DateFormatter
 
 from artist_sorting import ArtistSorter
 from artist_plotting import plot1, plot2
-
-
+from artist_utils import merge_files
 
 
 
@@ -30,27 +30,30 @@ def import_file(file_name):
 
 
 if __name__ == "__main__":
-
+    
+    # merge_files(folder_path="Artist_folder")
+    
 
     # Example usage
-    file_path = 'artist5_2021.csv'
+    file_path = 'artist_merged.csv'
     radar_processor = ArtistSorter(file_path)
     radar_processor.load_data()
     radar_processor.process_data()
     
     X_artist = radar_processor.processed_data
-    X_EISCAT = import_file(file_name="X_averaged_2021")
+    X_EISCAT = import_file(file_name="X_avg_test_data")
     
     
     
     
     for day in X_EISCAT:
-        plot2(X_EISCAT[day], X_artist[day])
-        break
+        plot1(X_artist[day])
+        # plot2(X_EISCAT[day], X_artist[day])
         
-        # for i in range(0, X_artist[day]['r_param'].shape[0]):
-        #     plt.plot(np.log10(X_artist[day]['r_param'][i]), X_artist[day]['r_h'].flatten())
-        # plt.show()
+        
+    #     # for i in range(0, X_artist[day]['r_param'].shape[0]):
+    #     #     plt.plot(np.log10(X_artist[day]['r_param'][i]), X_artist[day]['r_h'].flatten())
+    #     # plt.show()
     
 
 
