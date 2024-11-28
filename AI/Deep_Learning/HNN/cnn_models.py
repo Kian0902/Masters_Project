995321e2b1f4cc34c2cad298e36b1ec0a0ee1b84
Kian0902 <kian0902@hotmail.com>
2024-11-28 12:28:41 +0100

    Deleted unused code and cleaned up

--- AI/Deep_Learning/HNN/cnn_models.py
+++ AI/Deep_Learning/HNN/cnn_models.py
@@ -7,74 +7,6 @@ import torch.nn.functional as F
 # from utils import plot_ionogram
 
 
-class CNN(nn.Module):
-    def __init__(self):
-        super(CNN, self).__init__()
-
-        self.conv1 = nn.Sequential(
-            nn.Conv2d(in_channels=3, out_channels=16, kernel_size=3, stride=1, padding=1),
-            nn.BatchNorm2d(16),
-            nn.ReLU(),
-            nn.MaxPool2d(kernel_size=2, stride=2, padding=0)
-        )
-
-        self.conv2 = nn.Sequential(
-            nn.Conv2d(in_channels=16, out_channels=32, kernel_size=3, stride=1, padding=1),
-            nn.BatchNorm2d(32),
-            nn.ReLU(),
-            nn.MaxPool2d(kernel_size=2, stride=2, padding=0)
-        )
-
-        self.conv3 = nn.Sequential(
-            nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3, stride=1, padding=1),
-            nn.BatchNorm2d(64),
-            nn.ReLU(),
-            nn.MaxPool2d(kernel_size=2, stride=2, padding=0)
-        )
-        
-        self.conv4 = nn.Sequential(
-            nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3, stride=1, padding=1),
-            nn.BatchNorm2d(64),
-            nn.ReLU(),
-            
-        )
-        
-
-        self.fc1 = nn.Sequential(
-            
-            nn.Linear(64 * 10 * 10, 3200),
-            nn.BatchNorm1d(3200),
-            nn.ReLU(),
-            
-            nn.Linear(3200, 1600),
-            nn.BatchNorm1d(1600),
-            nn.ReLU(),
-            
-            nn.Linear(1600, 400),
-            nn.BatchNorm1d(400),
-            nn.ReLU(),
-            
-            nn.Linear(400, 27)
-        )
-
-        # self.fc2 = nn.Sequential(
-        #     nn.Linear(800, 27)
-        # )
-
-    def forward(self, x):
-        c1 = self.conv1(x)
-        c2 = self.conv2(c1)
-        c3 = self.conv3(c2)
-        c4 = self.conv4(c3)
-        
-        x_flat = c4.view(c4.size(0), -1)
-
-        x4 = self.fc1(x_flat)
-        return x4
-
-
-
-
 
 
 class BranchFNN(nn.Module):
@@ -108,8 +40,6 @@ class BranchFNN(nn.Module):
 
 
 
-
-
 class BranchCNN(nn.Module):
     def __init__(self):
         super(BranchCNN, self).__init__()
@@ -118,40 +48,39 @@ class BranchCNN(nn.Module):
             nn.Conv2d(in_channels=3, out_channels=16, kernel_size=3, stride=1, padding=1),
             nn.BatchNorm2d(16),
             nn.ReLU(),
-            nn.MaxPool2d(kernel_size=2, stride=2, padding=0)
+            nn.MaxPool2d(kernel_size=2, stride=2, padding=0)   # 81 --> 40
         )
 
         self.conv2 = nn.Sequential(
             nn.Conv2d(in_channels=16, out_channels=32, kernel_size=3, stride=1, padding=1),
             nn.BatchNorm2d(32),
             nn.ReLU(),
-            nn.MaxPool2d(kernel_size=2, stride=2, padding=0)
+            nn.MaxPool2d(kernel_size=2, stride=2, padding=0)   # 40 --> 20
         )
 
         self.conv3 = nn.Sequential(
-            nn.Conv2d(in_channels=32, out_channels=32, kernel_size=3, stride=1, padding=1),
-            nn.BatchNorm2d(32),
+            nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(64),
             nn.ReLU(),
-            #nn.MaxPool2d(kernel_size=2, stride=2, padding=0)
+            nn.MaxPool2d(kernel_size=2, stride=2, padding=0)   # 20 --> 10
         )
         
-        #self.conv4 = nn.Sequential(
-        #    nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3, stride=1, padding=1),
-        #    nn.BatchNorm2d(64),
-        #    nn.ReLU(),
-        #)
+        self.conv4 = nn.Sequential(
+            nn.Conv2d(in_channels=64, out_channels=128, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(128),
+            nn.ReLU(),
+        )
         
 
     def forward(self, x):
         c1 = self.conv1(x)
         c2 = self.conv2(c1)
         c3 = self.conv3(c2)
-        #c4 = self.conv4(c3)
+        c4 = self.conv4(c3)
         
-        x_flat = c3.view(c3.size(0), -1)
+        x_flat = c4.view(c4.size(0), -1)
 
         return x_flat
-
 
 
 
@@ -168,7 +97,7 @@ class CombinedNetwork(nn.Module):
 
         
         self.fc1 = nn.Sequential(
-            nn.Linear(32 * 20 * 20 + 1024, 6912),
+            nn.Linear(128 * 10 * 10 + 1024, 6912),
             nn.BatchNorm1d(6912),
             nn.ReLU(),
             )
@@ -211,92 +140,6 @@ class CombinedNetwork(nn.Module):
         return x
 
 
-
-
-
-
-
-
-# # Instantiate the model
-# model = CNN()
-# model.eval()
-# # Create a dummy input tensor with shape (1, 3, 81, 81)
-# dummy_input = torch.randn(1, 3, 81, 81)
-
-# # Pass the dummy input through the model
-# output = model(dummy_input)
-
-
-
-# # -*- coding: utf-8 -*-
-# """
-# Created on Mon Sep 30 11:16:01 2024
-
-# @author: Kian Sartipzadeh
-# """
-
-
-
-# import os
-# import torch
-# import torch.nn as nn
-# from torchvision import transforms
-
-# from utils import plot_ionogram
-
-
-
-
-
-# class CNN(nn.Module):
-#     def __init__(self):
-#         super(CNN, self).__init__()
-        
-        
-#         self.conv1 = nn.Sequential(
-#             nn.Conv2d(in_channels=3, out_channels=16, kernel_size=3, stride=1, padding=1),
-#             nn.BatchNorm2d(16),
-#             nn.ReLU(),
-#             nn.MaxPool2d(kernel_size=2, stride=2, padding=0)
-#             )
-        
-#         self.conv2 = nn.Sequential(
-#             nn.Conv2d(in_channels=16, out_channels=32, kernel_size=3, stride=1, padding=1),
-#             nn.BatchNorm2d(32),
-#             nn.ReLU(),
-#             nn.MaxPool2d(kernel_size=2, stride=2, padding=0)
-#             )
-        
-#         self.conv3 = nn.Sequential(
-#             nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3, stride=1, padding=1),
-#             nn.BatchNorm2d(64),
-#             nn.ReLU(),
-#             nn.MaxPool2d(kernel_size=2, stride=2, padding=0)
-#             )
-        
-        
-#         self.fc1 = nn.Sequential(
-#             nn.Linear(64*10*10, 800),
-#             nn.BatchNorm1d(800),
-#             nn.ReLU(),
-#             )
-        
-#         self.fc2 = nn.Sequential(
-#             nn.Linear(800, 27)
-#             )
-    
-#     def forward(self, x):
-        
-#         c1 = self.conv1(x)
-#         c2 = self.conv2(c1)
-#         c3 = self.conv3(c2)
-        
-#         x_flat = c3.view(c3.size(0), -1)
-        
-#         x4 = self.fc1(x_flat)
-#         x5 = self.fc2(x4)
-        
-#         return x5
 
 
 
