30dd05db0884b81cee2608e38e658c788f7f7613
Kian0902 <kian0902@hotmail.com>
2024-12-03 19:39:56 +0100

    Updated plotting

--- Artist/artist_utils.py
+++ Artist/artist_utils.py
@@ -8,6 +8,7 @@ Created on Wed Nov 27 15:58:51 2024
 
 
 import os
+import pickle
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -15,6 +16,18 @@ import matplotlib.pyplot as plt
 from datetime import datetime
 from matplotlib.dates import DateFormatter
 
+
+
+def load_data_dict(file_name):
+    with open(file_name, 'rb') as f:
+        dataset = pickle.load(f)
+    return dataset
+
+def save_data_dict(dataset: dict, file_name: str):
+    with open(file_name, 'wb') as file:
+        pickle.dump(dataset, file)
+    
+    print(f"Dict save as {file_name}")
 
 
 
@@ -46,7 +59,7 @@ def merge_files(folder_path):
 
 
 
-def plot1(data):
+def plot_data(data):
     """
     Plot a comparison of original and averaged data using pcolormesh.
 
@@ -60,33 +73,38 @@ def plot1(data):
                             for year, month, day, hour, minute, second in data['r_time']])
     r_h = data['r_h']
     r_param = data['r_param']
-    # r_error = data['r_error']
+    
+    
     
     # Date
     date_str = r_time[0].strftime('%Y-%m-%d')
     
     # Creating the plots
-    fig, ax = plt.subplots(1, 2, figsize=(10, 8))
-    fig.suptitle(f'Date: {date_str}', fontsize=15)
+    fig, ax = plt.subplots(1, 2, figsize=(12, 7), gridspec_kw={'width_ratios': [0.6, 1]}, sharey=True)
+    fig.suptitle(f'Date: {date_str}', fontsize=20, y=1.02)
     fig.tight_layout()
+    # fig.autofmt_xdate()
     
+    ax[0].set_title('All measurements', fontsize=17)
+    ax[0].set_xlabel(r'$log_{10}(n_e)$ [n/cm$^3$]', fontsize=13)
+    ax[0].set_ylabel('Altitude [km]', fontsize=15)
+    ax[0].grid(True)
+    ax[0].tick_params(axis='x', rotation=0)
+    for i in range(0, r_param.shape[1]):
+        
+        ax[0].plot(np.log10(r_param[:, i].T), r_h.flatten())
+        
     
     # Plotting original data
-    pcm_ne=ax[0].pcolormesh(r_time, r_h.flatten(), np.log10(r_param), shading='auto', cmap='turbo', vmin=9, vmax=12)
-    ax[0].set_title('EISCAT UHF', fontsize=17)
-    ax[0].set_xlabel('Time [hours]')
-    ax[0].set_ylabel('Altitude [km]')
-    ax[0].xaxis.set_major_formatter(DateFormatter('%H:%M'))
-    fig.autofmt_xdate()
+    pcm_ne=ax[1].pcolormesh(r_time, r_h.flatten(), np.log10(r_param), shading='auto', cmap='turbo', vmin=10, vmax=12)
+    ax[1].set_title('EISCAT UHF', fontsize=17)
+    ax[1].set_xlabel('Time [hours]', fontsize=13)
+    ax[1].xaxis.set_major_formatter(DateFormatter('%H:%M'))
+    ax[1].tick_params(axis='x', rotation=45)
     
     # Add colorbar for the original data
-    cbar = fig.colorbar(pcm_ne, ax=ax[0], orientation='vertical')
-    cbar.set_label(r'$log_{10}(n_e)$ [g/cm$^3$]', fontsize=17)
-    
-    for i in range(0, r_param.shape[0]):
-        
-        ax[1].plot(np.log10(r_param[i].T), r_h.flatten())
-
+    cbar = fig.colorbar(pcm_ne, ax=ax[1], orientation='vertical')
+    cbar.set_label(r'$log_{10}(n_e)$ [n/cm$^3$]', fontsize=17)
     
     
     # Display the plots
@@ -97,7 +115,7 @@ def plot1(data):
 
 
 
-def plot2(data1, data2):
+def plot_eiscat_vs_artist(X_eiscat, X_artist):
     """
     Plot a comparison of original and averaged data using pcolormesh.
 
@@ -108,14 +126,14 @@ def plot2(data1, data2):
     
     # Convert time arrays to datetime objects
     r_time1 = np.array([datetime(year, month, day, hour, minute, second) 
-                            for year, month, day, hour, minute, second in data1['r_time']])
+                            for year, month, day, hour, minute, second in X_eiscat['r_time']])
     # Convert time arrays to datetime objects
     r_time2 = np.array([datetime(year, month, day, hour, minute, second) 
-                            for year, month, day, hour, minute, second in data2['r_time']])
-    r_h1 = data1['r_h']
-    r_h2 = data2['r_h']
-    r_param1 = data1['r_param']
-    r_param2 = data2['r_param']
+                            for year, month, day, hour, minute, second in X_artist['r_time']])
+    r_h1 = X_eiscat['r_h']
+    r_h2 = X_artist['r_h']
+    r_param1 = X_eiscat['r_param']
+    r_param2 = X_artist['r_param']
     
     
     # Date
@@ -124,7 +142,7 @@ def plot2(data1, data2):
     
     # Creating the plots
     fig, ax = plt.subplots(1, 2, figsize=(10, 8), sharey=True)
-    fig.suptitle(f'Date: {date_str}', fontsize=15)
+    fig.suptitle(f'Date: {date_str}', fontsize=20)
     fig.tight_layout()
     fig.autofmt_xdate()
     x_limits = [r_time1[0], r_time1[-1]]
@@ -133,21 +151,21 @@ def plot2(data1, data2):
     # Plotting original data
     ne_EISCAT = ax[0].pcolormesh(r_time1, r_h1.flatten(), np.log10(r_param1), shading='auto', cmap='turbo', vmin=10, vmax=12)
     ax[0].set_title('EISCAT UHF', fontsize=17)
-    ax[0].set_xlabel('Time [hours]')
-    ax[0].set_ylabel('Altitude [km]')
+    ax[0].set_xlabel('Time [hours]', fontsize=13)
+    ax[0].set_ylabel('Altitude [km]', fontsize=15)
     ax[0].xaxis.set_major_formatter(DateFormatter('%H:%M'))
     
     
     # Plotting original data
     ne_Artist = ax[1].pcolormesh(r_time2, r_h2.flatten(), np.log10(r_param2), shading='auto', cmap='turbo', vmin=10, vmax=12)
     ax[1].set_title('Artist', fontsize=17)
-    ax[1].set_xlabel('Time [hours]')
+    ax[1].set_xlabel('Time [hours]', fontsize=13)
     ax[1].xaxis.set_major_formatter(DateFormatter('%H:%M'))
     ax[1].set_xlim(x_limits)
     
     # Add colorbar for the original data
     cbar = fig.colorbar(ne_EISCAT, ax=ax[1], orientation='vertical', fraction=0.03, pad=0.04, aspect=44, shrink=3)
-    cbar.set_label(r'$log_{10}(n_e)$ [g/cm$^3$]', fontsize=17)
+    cbar.set_label(r'$log_{10}(n_e)$ [n/cm$^3$]', fontsize=17)
     
     
     # Display the plots
