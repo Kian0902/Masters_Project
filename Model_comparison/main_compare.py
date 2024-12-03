8129521372a51c9f0e17548d3c78a057f82ebfec
Kian0902 <kian0902@hotmail.com>
2024-12-03 17:04:02 +0100

    Updated filename

--- Model_comparison/main_compare.py
+++ Model_comparison/main_compare.py
@@ -18,9 +18,9 @@ from torch.utils.data import DataLoader
 from storing_dataset import Matching3Pairs, Store3Dataset
 
 
-from eval_utils import from_csv_to_numpy, from_strings_to_datetime, plot_compare
+from eval_utils import from_csv_to_numpy, from_strings_to_datetime, plot_compare, plot_compare_r2, plot_results
 from hnn_model import CombinedNetwork
-
+from sklearn.metrics import r2_score
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 print(device)
@@ -29,13 +29,13 @@ print(device)
 
 
 
-
+s = "3"
 
 # Test data folder names
-test_ionogram_folder = "testing_data/test_ionogram_folder - Copy"
-test_artist_folder = "testing_data/test_artist_folder - Copy"
-test_radar_folder = "testing_data/test_radar_folder - Copy"        # These are the true data
-test_sp19_folder = "testing_data/test_sp19_folder - Copy"
+test_ionogram_folder = "testing_data/test_ionogram_folder_" + s
+# test_artist_folder = "testing_data/test_artist_folder_1"
+test_radar_folder = "testing_data/test_eiscat_folder_" + s        # These are the true data
+test_sp19_folder = "testing_data/test_geophys_folder_" + s
 
 
 
@@ -50,9 +50,9 @@ rad, ion, sp, radar_times = Pairs.find_pairs(return_date=True)
 r_times = from_strings_to_datetime(radar_times)
 
 
-# Processing Artist sample files
-art, artist_times = from_csv_to_numpy(test_artist_folder)
-art_time = from_strings_to_datetime(artist_times)  # convert filenames to datetimes
+# # Processing Artist sample files
+# art, artist_times = from_csv_to_numpy(test_artist_folder)
+# art_time = from_strings_to_datetime(artist_times)  # convert filenames to datetimes
 
 
 # art_h = np.arange(80, 485, 5)
@@ -80,7 +80,7 @@ model = CombinedNetwork()
 criterion = nn.HuberLoss()
 
 # Loading the trained network weights
-weights_path = 'best_model_weights.pth'
+weights_path = 'HNN_v1_best_weights.pth'
 model.load_state_dict(torch.load(weights_path, weights_only=True))
 model.to(device)
 
@@ -113,14 +113,22 @@ with torch.no_grad():
 model_ne = np.array(predictions)
 eiscat_ne = np.array(true_targets)
 
+# Calculate R² score for each feature
+r2_scores = [r2_score(eiscat_ne[:, i], model_ne[:, i]) for i in range(model_ne.shape[1])]
+
+# Print R² scores for all features
+for i, r2 in enumerate(r2_scores):
+    print(f"R² score for feature {i + 1}: {r2:.4f}")
 
 
 
-plot_compare(eiscat_ne, model_ne, np.log10(art), r_times, art_time)
+
+plot_compare_r2(eiscat_ne, model_ne, r2_scores, r_times)
 
 
-
-
+# for i in range(0, eiscat_ne.shape[0]):
+#     plot_results(model_ne[i], eiscat_ne[i])
+    
 
 
 
