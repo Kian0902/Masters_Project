23da9602e20a7188d8b28c7e0cbefba3be3d3eda
Kian0902 <kian0902@hotmail.com>
2024-12-03 17:05:00 +0100

    Added new function for plotting Eiscat vs HNN model with a narrow plot of R2 scores for each altitude, in the middle of the two.

--- Model_comparison/eval_utils.py
+++ Model_comparison/eval_utils.py
@@ -78,11 +78,107 @@ def from_csv_to_numpy(folder):
 
 
 
+def plot_compare(ne_true, ne_pred, r_time):
+    
+    
+    # Eiscat altitudes
+    r_h = np.array([[91.5687711 ],[ 94.57444598],[ 97.57964223],[100.57010953],
+           [103.57141624],[106.57728701],[110.08393175],[114.60422289],
+           [120.1185208 ],[126.61221111],[134.1346149 ],[142.53945817],
+           [152.05174717],[162.57986185],[174.09833378],[186.65837945],
+           [200.15192581],[214.62769852],[230.12198695],[246.64398082],
+           [264.11728204],[282.62750673],[302.15668686],[322.70723831],
+           [344.19596481],[366.64409299],[390.113117]])
+    
+
+    date_str = r_time[0].strftime('%Y-%m-%d')
+
+
+    # Creating the plots
+    fig, ax = plt.subplots(1, 2, figsize=(12, 6), sharey=True)
+    fig.suptitle(f'Date: {date_str}', fontsize=20)
+    fig.tight_layout()
+    
+    
+    # Plotting original data
+    ne_EISCAT = ax[0].pcolormesh(r_time, r_h.flatten(), ne_true.T, shading='auto', cmap='turbo', vmin=10, vmax=12)
+    ax[0].set_title('EISCAT UHF', fontsize=17)
+    ax[0].set_xlabel('Time [hours]', fontsize=13)
+    ax[0].set_ylabel('Altitude [km]', fontsize=15)
+    ax[0].xaxis.set_major_formatter(DateFormatter('%H:%M'))
+    
+    # Plotting predicted data
+    ne_HNN = ax[1].pcolormesh(r_time, r_h.flatten(), ne_pred.T, shading='auto', cmap='turbo', vmin=10, vmax=12)
+    ax[1].set_title('HNN', fontsize=17)
+    ax[1].set_xlabel('Time [hours]', fontsize=13)
+    ax[1].xaxis.set_major_formatter(DateFormatter('%H:%M'))
+    
+    
+    # Add colorbar for the predicted data
+    cbar = fig.colorbar(ne_HNN, ax=ax[1], orientation='vertical', fraction=0.048, pad=0.04)
+    cbar.set_label(r'$log_{10}(n_e)$ [g/cm$^3$]', fontsize=17, labelpad=15)
+    
+    fig.autofmt_xdate()
+    plt.show()
 
 
 
 
-def plot_compare(ne_true, ne_pred, ne_art, r_time, art_time):
+def plot_compare_r2(ne_true, ne_pred, r2_scores, r_time):
+    
+    
+    # Eiscat altitudes
+    r_h = np.array([[91.5687711 ],[ 94.57444598],[ 97.57964223],[100.57010953],
+           [103.57141624],[106.57728701],[110.08393175],[114.60422289],
+           [120.1185208 ],[126.61221111],[134.1346149 ],[142.53945817],
+           [152.05174717],[162.57986185],[174.09833378],[186.65837945],
+           [200.15192581],[214.62769852],[230.12198695],[246.64398082],
+           [264.11728204],[282.62750673],[302.15668686],[322.70723831],
+           [344.19596481],[366.64409299],[390.113117]])
+    
+
+    date_str = r_time[0].strftime('%Y-%m-%d')
+
+    
+    # Creating the plots
+    fig, ax = plt.subplots(1, 3, figsize=(16, 6), gridspec_kw={'width_ratios': [1, 0.2, 1]}, sharey=True)
+    fig.suptitle(f'Date: {date_str}', fontsize=20, y=1.02)
+    fig.tight_layout()
+
+    # Plotting original data
+    ne_EISCAT = ax[0].pcolormesh(r_time, r_h, ne_true.T, shading='auto', cmap='turbo', vmin=10, vmax=12)
+    ax[0].set_title('EISCAT UHF', fontsize=17)
+    ax[0].set_xlabel('Time [hours]', fontsize=13)
+    ax[0].set_ylabel('Altitude [km]', fontsize=15)
+    ax[0].xaxis.set_major_formatter(DateFormatter('%H:%M'))
+
+    # Plotting R2-scores as a line plot
+    ax[1].plot(r2_scores, r_h, color='C0', label=r'$R^2$')
+    ax[1].set_title(r'$R^2$ Scores', fontsize=17)
+    ax[1].set_xlabel(r'$R^2$', fontsize=13)
+    ax[1].grid()
+    ax[1].legend()
+    ax[1].set_xlim(xmin=-0.1, xmax=1.1)
+    
+    # Plotting predicted data
+    ne_HNN = ax[2].pcolormesh(r_time, r_h, ne_pred.T, shading='auto', cmap='turbo', vmin=10, vmax=12)
+    ax[2].set_title('HNN', fontsize=17)
+    ax[2].set_xlabel('Time [hours]', fontsize=13)
+    ax[2].xaxis.set_major_formatter(DateFormatter('%H:%M'))
+
+    # Add colorbar for the predicted data
+    cbar = fig.colorbar(ne_HNN, ax=ax[2], orientation='vertical', fraction=0.048, pad=0.04)
+    cbar.set_label(r'$log_{10}(n_e)$ [g/cm$^3$]', fontsize=17, labelpad=15)
+
+    fig.autofmt_xdate()
+    plt.show()
+
+
+
+
+
+
+def plot_compare_all(ne_true, ne_pred, ne_art, r_time, art_time):
     
     
     
