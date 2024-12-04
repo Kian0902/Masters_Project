8c99ef121995197f14dc50cf84392d706b4effb5
Kian0902 <kian0902@hotmail.com>
2024-12-04 20:16:50 +0100

    Made code for plotting ionograms

---
+++ Ionogram/plot_ionograms.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov 25 11:51:14 2024
+
+@author: Kian Sartipzadeh
+"""
+
+
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+from ionogram_sorting import IonogramSorting
+from process_ionograms import IonogramProcessing
+
+
+
+
+
+
+
+
+datapath_folder = "Ionogram_TXT"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
