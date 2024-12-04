8ed6e79f5abdc44c041e294ae19b679b0071cc67
Kian0902 <kian0902@hotmail.com>
2024-12-04 16:13:16 +0100

    Added code for running interpolation function

--- Artist/main_artist.py
+++ Artist/main_artist.py
@@ -11,8 +11,7 @@ import pandas as pd
 
 from artist_sorting import ArtistSorter
 from artist_utils import save_data_dict, load_data_dict, merge_files, plot_data, plot_eiscat_vs_artist
-
-
+from artist_interpolate import filter_range, interpolate_data
 
 
 
@@ -36,20 +35,51 @@ if __name__ == "__main__":
     # save_data(X_artist, file_name="artist_processed_data")
     # ------------------------------------------------------
     
-    
-    
-    
-    X_artist = load_data_dict("artist_processed_data")
-    X_EISCAT = load_data_dict(file_name="X_avg_test_data")
-    
-    
-    combined_artist_data = {}  # Dictionary to store all corresponding X_artist data
 
-    for day in X_EISCAT:
-        plot_eiscat_vs_artist(X_EISCAT[day], X_artist[day])  # Optional, if visualization is required
-        combined_artist_data[day] = X_artist[day]  # Add data for the current day to the dictionary
     
-    # Save the combined data to a single file
-    save_data_dict(combined_artist_data, file_name="artist_test_days.pkl")
+    # --- Uncomment for processing all artist file ---
+    # X_artist = load_data_dict("artist_processed_data")
+    # X_EISCAT = load_data_dict(file_name="X_avg_test_data")
+    
+    
+    # combined_artist_data = {}  # Dictionary to store all corresponding X_artist data
+
+    # for day in X_EISCAT:
+    #     plot_eiscat_vs_artist(X_EISCAT[day], X_artist[day])  # Optional, if visualization is required
+    #     combined_artist_data[day] = X_artist[day]  # Add data for the current day to the dictionary
+    
+    # # Save the combined data to a single file
+    # save_data_dict(combined_artist_data, file_name="artist_test_days.pkl")
 
     # print("All artist data saved to combined_artist_data.pkl")
+    # ------------------------------------------------------
+    
+    
+    
+    
+    
+    X_artist = load_data_dict("artist_test_days.pkl")
+    X_EISCAT = load_data_dict(file_name="X_avg_test_data")
+
+
+    artist_processed = {}
+
+    for day in X_artist:
+        X_uhf = X_EISCAT[day]
+        X_art = X_artist[day]
+        # plot_data(X)
+        r_uhf = X_uhf['r_h']
+        # plot_eiscat_vs_artist(X_uhf, X_art)
+        
+        X_filt = filter_range(X_art, 'r_h', 90, 400)
+        # plot_eiscat_vs_artist(X_uhf, X_filt)
+        
+        
+        X_inter = interpolate_data(X_filt, r_uhf)
+        plot_eiscat_vs_artist(X_uhf, X_inter)
+        artist_processed[day] = X_artist[day]
+    
+    save_data_dict(artist_processed, file_name="processed_artist_test_days.pkl")
+    
+    
+    
