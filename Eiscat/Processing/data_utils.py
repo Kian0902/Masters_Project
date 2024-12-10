898f2df4eb76f85e6f9ac2c6f787d631dcb95a66
Kian0902 <kian0902@hotmail.com>
2024-12-10 17:26:40 +0100

    Made python file for containing support functions

---
+++ Eiscat/Processing/data_utils.py
@@ -0,0 +1,128 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Dec 10 17:24:01 2024
+
+@author: Kian Sartipzadeh
+"""
+
+
+import pickle
+import numpy as np
+from datetime import datetime
+
+def save_data(dataset: dict, file_name: str):
+    with open(file_name, 'wb') as file:
+        pickle.dump(dataset, file)
+
+
+
+
+def detect_nan_in_arrays(data_dict):
+    for key, array in data_dict.items():
+        if np.isnan(array).any():
+            print(f"NaN detected {key}: {array.shape}")
+
+
+
+
+
+def load_dict(file_name):
+    with open(file_name, 'rb') as f:
+        dataset = pickle.load(f)
+    return dataset
+
+def save_dict(dataset: dict, file_name: str):
+    with open(file_name, 'wb') as file:
+        pickle.dump(dataset, file)
+
+
+
+
+def from_strings_to_datetime(data_strings):
+    data = from_strings_to_array(data_strings)
+    
+    # Convert time arrays to datetime objects
+    r_time = np.array([datetime(year, month, day, hour, minute, second) 
+                            for year, month, day, hour, minute, second in data])
+    
+    return r_time
+
+
+def from_array_to_datetime(data):
+    # Convert time arrays to datetime objects
+    r_time = np.array([datetime(year, month, day, hour, minute, second) 
+                            for year, month, day, hour, minute, second in data])
+    
+    return r_time
+
+
+
+# Function to preprocess the input strings
+def from_strings_to_array(date_strings):
+    result = []
+    for date_string in date_strings:
+        date_part, time_part = date_string.split("_")
+        year = int(date_part[:4])
+        month = int(date_part[4:6])
+        day = int(date_part[6:8])
+        hour = int(time_part[:2])
+        minute = int(time_part[2:4])
+        second = 0  # Assuming seconds are always zero
+        result.append(np.array([year, month, day, hour, minute, second]))
+        
+    return result
+
+
+
+
+
+
+
+
+def inspect_dict(d, indent=0):
+    """
+    Recursively print all keys in a nested dictionary with the shape of their values.
+    
+    :param d: The dictionary to process.
+    :param indent: The current indentation level (used for nested items).
+    """
+    for key, value in d.items():
+        # Create indentation based on the depth level
+        prefix = '  ' * indent
+        
+        # Determine the type and shape of the value
+        if isinstance(value, dict):
+            value_shape = f"dict with {len(value)} keys"
+        elif isinstance(value, list):
+            value_shape = f"list of length {len(value)}"
+        elif isinstance(value, set):
+            value_shape = f"set of length {len(value)}"
+        elif isinstance(value, tuple):
+            value_shape = f"tuple of length {len(value)}"
+        elif isinstance(value, np.ndarray):
+            value_shape = f"numpy array with shape {value.shape}"
+        else:
+            value_shape = f"type {type(value).__name__}"
+        
+        # Print the key along with its value shape
+        print(f"{prefix}{key}: ({value_shape})")
+        
+        # If the value is also a dictionary, recursively call the function
+        if isinstance(value, dict):
+            inspect_dict(value, indent=indent + 1)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
