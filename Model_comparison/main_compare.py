3cb19a309c225ef7d1b654f41d589dfa72fbd554
Kian0902 <kian0902@hotmail.com>
2024-12-10 14:57:51 +0100

    Mooved code from applying kian-net, to eval_predict

--- Model_comparison/main_compare.py
+++ Model_comparison/main_compare.py
@@ -13,71 +13,23 @@ from storing_dataset import Matching3Pairs, Store3Dataset
 
 from eval_utils import save_dict, load_dict, apply_log10, revert_log10, add_key_from_dict_to_dict, convert_pred_to_dict, convert_ionograms_to_dict, from_csv_to_numpy, from_strings_to_array, from_strings_to_datetime, filter_artist_times, inspect_dict
 from eval_plotting import RadarPlotter
-from eval_predict import apply_model
+from eval_predict import model_predict
 from eval_peaks import IonosphericPeakFinder
 from hnn_model import CombinedNetwork
 
 
 
-# Test data folder names
-test_ionogram_folder = "testing_data/test_ionogram_folder"
-test_radar_folder = "testing_data/test_eiscat_folder"        # These are the true data
-test_sp19_folder = "testing_data/test_geophys_folder"
-
-
-
-
-
-# Initializing class for matching pairs
-Pairs = Matching3Pairs(test_ionogram_folder, test_radar_folder, test_sp19_folder)
-
-
-# Returning matching sample pairs
-rad, ion, sp, radar_times = Pairs.find_pairs(return_date=True)
-r_t = from_strings_to_datetime(radar_times)
-r_times = from_strings_to_array(radar_times)
-
-rad = np.abs(rad)
-rad[rad < 1e5] = 1e6
-
-
-# Storing the sample pairs
-A = Store3Dataset(ion, sp, np.log10(rad), transforms.Compose([transforms.ToTensor()]))
-
-# Path to trained weights
-weights_path = 'HNN_v1_best_weights.pth'
-
-
-
-X_pred = apply_model(A, CombinedNetwork(), weights_path)
-X_hnn = convert_pred_to_dict(r_t, r_times, X_pred)
-
-
-X_true = from_csv_to_numpy(test_radar_folder)[0]
-X_eis = convert_pred_to_dict(r_t, r_times, X_true)
-
-
-X_art = load_dict("processed_artist_test_days.pkl")
-X_art = filter_artist_times(X_eis, X_hnn, X_art)
-
-
-# Adding 'r_h' from eiscat to all dicts
-Eiscat_support = load_dict("X_avg_test_data")
-X_eis = add_key_from_dict_to_dict(Eiscat_support, X_eis)
-X_hnn = add_key_from_dict_to_dict(Eiscat_support, X_hnn)
-X_art = add_key_from_dict_to_dict(Eiscat_support, X_art)
-
-
-
-
-X_ion = convert_ionograms_to_dict(ion, X_eis)
+X_eis = load_dict("X_eis.pkl")
+X_kian = load_dict("X_kian.pkl")
+X_art = load_dict("X_art.pkl")
+X_ion = load_dict("X_ion.pkl")
 
 
 eis_peak_finder = IonosphericPeakFinder(X_eis)
 X_eis = eis_peak_finder.get_peaks()
 
-hnn_peak_finder = IonosphericPeakFinder(X_hnn)
-X_hnn = hnn_peak_finder.get_peaks()
+kian_peak_finder = IonosphericPeakFinder(X_kian)
+X_kian = kian_peak_finder.get_peaks()
 
 art_peak_finder = IonosphericPeakFinder(X_art)
 X_art = art_peak_finder.get_peaks()
@@ -95,8 +47,8 @@ selected_datetimes = from_strings_to_datetime(selected_dates_and_times)
 
 
 
-for day in X_hnn:
-    radar_plotter = RadarPlotter(X_eis[day], X_hnn[day], X_art[day], X_ion[day])
+for day in X_kian:
+    radar_plotter = RadarPlotter(X_eis[day], X_kian[day], X_art[day], X_ion[day])
     radar_plotter.plot_compare_all_interactive()
     # break
     # radar_plotter.plot_all_peaks()
