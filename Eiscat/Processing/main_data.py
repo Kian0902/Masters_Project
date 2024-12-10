4a31f8dd652f1c06932d5499313d3f305ed84f05
Kian0902 <kian0902@hotmail.com>
2024-12-10 21:42:27 +0100

    Tested

--- Eiscat/Processing/main_data.py
+++ Eiscat/Processing/main_data.py
@@ -16,107 +16,46 @@ from read_EISCAT_data import EISCATDataProcessor
 from data_sorting import EISCATDataSorter
 from data_averaging import EISCATAverager
 from data_filtering import EISCATDataFilter
+from data_plotting import EISCATPlotter
 from data_outlier_detection import EISCATOutlierDetection
 from matplotlib.dates import DateFormatter
-
+from data_utils import from_array_to_datetime, inspect_dict, get_day, get_day_data, MatchingFiles
 from scipy.interpolate import interp1d
 import numpy as np
 
 
 
 
-def plot(data):
-    """
-    Plot a comparison of original and averaged data using pcolormesh.
-
-    Input (type)                 | DESCRIPTION
-    ------------------------------------------------
-    original_data (dict)         | Dictionary containing the original data.
-    """
-    
-    # Convert time arrays to datetime objects
-    r_time = np.array([datetime(year, month, day, hour, minute, second) 
-                            for year, month, day, hour, minute, second in data['r_time']])
-    r_h = data['r_h']
-    r_param = data['r_param']
-    r_error = data['r_error']
-    
-    # Date
-    date_str = r_time[0].strftime('%Y-%m-%d')
-    
-    # Creating the plots
-    fig, ax = plt.subplots(1, 2, figsize=(10, 8), sharey=True)
-    fig.suptitle(f'Date: {date_str}', fontsize=15)
-    fig.tight_layout()
-    
-    
-    # Plotting original data
-    pcm_ne = ax[0].pcolormesh(r_time, r_h.flatten(), np.log10(r_param), shading='auto', cmap='turbo', vmin=9, vmax=12)
-    ax[0].set_title('EISCAT UHF', fontsize=17)
-    ax[0].set_xlabel('Time [hours]')
-    ax[0].set_ylabel('Altitude [km]')
-    ax[0].xaxis.set_major_formatter(DateFormatter('%H:%M'))
-    fig.autofmt_xdate()
-    
-    # Add colorbar for the original data
-    # cbar = fig.colorbar(pcm_ne, ax=ax[0], orientation='vertical', fraction=0.03, pad=0.04, aspect=20, shrink=1)
-    # cbar.set_label('log10(n_e) [g/cm^3]')
-    
-    # fig.autofmt_xdate()
-    
-    # Plotting original data
-    pcm_err = ax[1].pcolormesh(r_time, r_h.flatten(), np.log10(r_error), shading='auto', cmap='turbo', vmin=9, vmax=12)
-    ax[1].set_title('Measurement Error', fontsize=17)
-    ax[1].set_xlabel('Time [hours]')
-    # ax[1].set_ylabel('Altitude [km]')
-    ax[1].xaxis.set_major_formatter(DateFormatter('%H:%M'))
-    # fig.autofmt_xdate()
-    
-    # Add colorbar for the original data
-    cbar = fig.colorbar(pcm_err, ax=ax[1], orientation='vertical', fraction=0.03, pad=0.04, aspect=44, shrink=3)
-    cbar.set_label(r'$log_{10}(n_e)$ [g/cm$^3$]', fontsize=17)
-    
-    
-    # Display the plots
-    plt.show()
-
-
-
-
-
-def save_data(dataset: dict, file_name: str):
-    with open(file_name, 'wb') as file:
-        pickle.dump(dataset, file)
-
-
-
-def detect_nan_in_arrays(data_dict):
-    for key, array in data_dict.items():
-        if np.isnan(array).any():
-            print(f"NaN detected {key}: {array.shape}")
-
-
 # # Use the local folder name containing data
-# folder_name_in  = "EISCAT_Madrigal/selected_test_eiscat"
-folder_name_out = "EISCAT_MAT/EISCAT_test_data"
-# folder_name_out = "EISCAT_MAT/All"
+# folder_name_in  = "EISCAT_Madrigal/UHF_All"
+# folder_name_out = "EISCAT_MAT/UHF_All"
 
-# Extract info from hdf5 files
+# # Extract info from hdf5 files
 # madrigal_processor = EISCATDataProcessor(folder_name_in, folder_name_out)
 # madrigal_processor.process_all_files()
 
 
 
+# VHF_folder = "EISCAT_MAT/VHF_All"
+UHF_folder = "EISCAT_MAT/UHF_All"
+
+both_folder = "EISCAT_MAT/All"
+
+
+# Match = MatchingFiles(VHF_folder, UHF_folder)
+# Match.remove_matching_vhf_files()
+
+
+
 # Sorting data
-Eiscat = EISCATDataSorter(folder_name_out)
+Eiscat = EISCATDataSorter(both_folder)
 Eiscat.sort_data()               # sort data
 X_Eiscat = Eiscat.return_data()  # returning dict data
 
 
-# x = X_Eiscat["2020-1-14"]
 
-# plot(x)
-
+radar_plotter = EISCATPlotter(get_day_data(X_Eiscat, 2))
+radar_plotter.plot_day_temp()
 
 
 # Clipping range and Filtering data for nan
@@ -125,10 +64,22 @@ filt.batch_filtering()
 X_filt = filt.return_data()
 
 
+radar_plotter = EISCATPlotter(get_day_data(filt.return_data(), 2))
+radar_plotter.plot_day_temp()
+
+
+
+# X = X_filt["2019-3-19"]
+
+# radar_plotter = EISCATPlotter(X)
+# radar_plotter.plot_day_temp()
+
+
 # Detecting outliers
 Outlier = EISCATOutlierDetection(X_filt)
 Outlier.batch_detection(method_name="IQR", save_plot=False)
 X_outliers = Outlier.return_outliers()
+
 
 
 # Filtering outliers
@@ -136,7 +87,8 @@ outlier_filter = EISCATDataFilter(X_filt, filt_outlier=True)
 outlier_filter.batch_filtering(dataset_outliers=X_outliers, filter_size=3, plot_after_each_day=False)
 X_outliers_filtered = outlier_filter.return_data()
 
-
+radar_plotter = EISCATPlotter(get_day_data(outlier_filter.return_data(), 2))
+radar_plotter.plot_day_temp()
 
 
 # Assuming X_avg contains your nested dictionary after filtering and averaging
@@ -158,26 +110,30 @@ for date, data in X_outliers_filtered.items():
     r_h = data["r_h"].flatten()  # Original altitudes (shape: (N,))
     r_param = data["r_param"]     # Electron density measurements (shape: (N, M))
     r_error = data["r_error"]     # Error values (shape: (N, M))
-    
+    r_systemp = data["r_systemp"]
     
     # Interpolating electron density (r_param) to reference_altitudes
     interpolated_r_param = np.zeros((len(reference_altitudes), r_param.shape[1]))
     interpolated_r_error = np.zeros((len(reference_altitudes), r_error.shape[1]))
+    interpolated_r_systemp = np.zeros((len(reference_altitudes), r_systemp.shape[1]))
     
     for i in range(r_param.shape[1]):
         # Interpolating each column of r_param and r_error
         interpolated_r_param[:, i] = np.interp(reference_altitudes, r_h, r_param[:, i])
         interpolated_r_error[:, i] = np.interp(reference_altitudes, r_h, r_error[:, i])
-
+        interpolated_r_systemp[:, i] = np.interp(reference_altitudes, r_h, r_systemp[:, i])
+        
     # Storing the interpolated values in the new dictionary
     X_interp[date] = {
         "r_time": data["r_time"],  # Keep the original time data
         "r_h": reference_altitudes.reshape(-1, 1),  # Use the reference altitudes (shape: (27, 1))
         "r_param": interpolated_r_param,            # Interpolated electron densities (shape: (27, M))
-        "r_error": interpolated_r_error             # Interpolated errors (shape: (27, M))
+        "r_error": interpolated_r_error,             # Interpolated errors (shape: (27, M))
+        "r_systemp": interpolated_r_systemp
     }
 
 # X_interp now contains the data with all days having 27 altitude levels
+
 
 # Averaging data
 AVG = EISCATAverager(X_interp)
@@ -185,13 +141,29 @@ AVG.batch_averaging(save_plot=False, weighted=False)
 X_avg = AVG.return_data()
 
 
-for day in X_avg:
-    detect_nan_in_arrays(X_avg[day])
+
+# X = X_avg["2022-9-2"]
+
+
+
+inspect_dict(X_filt["2022-9-2"])
+radar_plotter = EISCATPlotter(X_filt["2022-9-2"])
+radar_plotter.plot_day()
+
+
+inspect_dict(X_avg["2022-9-2"])
+radar_plotter = EISCATPlotter(X_avg["2022-9-2"])
+radar_plotter.plot_day()
+
+
+
+# for day in X_avg:
+#     detect_nan_in_arrays(X_avg[day])
 
 
 
 
-save_data(X_avg, file_name="X_avg_test_data")
+# save_data(X_avg, file_name="X_avg_test_data")
 
 
 
