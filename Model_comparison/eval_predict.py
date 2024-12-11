3eb5907787dcedab07953c7f44cab787c3186b07
Kian0902 <kian0902@hotmail.com>
2024-12-11 23:24:16 +0100

    f

--- Model_comparison/eval_predict.py
+++ Model_comparison/eval_predict.py
@@ -61,6 +61,7 @@ def model_predict(stored_dataset, DL_model, model_weights):
             outputs = model(data1, data2)
             
             loss = criterion(outputs, targets)
+            print(loss)
             predictions.extend(outputs.cpu().numpy())
     
     
@@ -73,9 +74,9 @@ def model_predict(stored_dataset, DL_model, model_weights):
 if __name__ == "__main__":
 
     # Test data folder names
-    test_ionogram_folder = "testing_data/test_ionogram_folder"
-    test_radar_folder = "testing_data/test_eiscat_folder"        # These are the true data
-    test_sp19_folder = "testing_data/test_geophys_folder"
+    test_ionogram_folder = "testing_data/test_ionogram_folder_E+F"
+    test_radar_folder = "testing_data/test_eiscat_folder_E+F"        # These are the true data
+    test_sp19_folder = "testing_data/test_geophys_folder_E+F"
 
 
     # Initializing class for matching pairs
@@ -100,38 +101,38 @@ if __name__ == "__main__":
 
 
     X_pred = model_predict(A, CombinedNetwork(), weights_path)
-    X_kian = convert_pred_to_dict(r_t, r_times, X_pred)
+    # X_kian = convert_pred_to_dict(r_t, r_times, X_pred)
     
     
-    X_true = from_csv_to_numpy(test_radar_folder)[0]
-    X_eis = convert_pred_to_dict(r_t, r_times, X_true)
+    # X_true = from_csv_to_numpy(test_radar_folder)[0]
+    # X_eis = convert_pred_to_dict(r_t, r_times, X_true)
     
     
-    print(inspect_dict(X_eis))
+    # # print(inspect_dict(X_eis))
     
     
-    X_art = load_dict("processed_artist_test_days.pkl")
-    X_art = filter_artist_times(X_eis, X_kian, X_art)
+    # X_art = load_dict("processed_artist_test_days.pkl")
+    # X_art = filter_artist_times(X_eis, X_kian, X_art)
     
     
-    # Adding 'r_h' from eiscat to all dicts
-    Eiscat_support = load_dict("X_avg_test_data")
-    X_eis = add_key_from_dict_to_dict(Eiscat_support, X_eis, key="r_h")
-    X_eis = add_key_with_matching_times(Eiscat_support, X_eis, key="r_error")
+    # # Adding 'r_h' from eiscat to all dicts
+    # Eiscat_support = load_dict("X_avg_test_data")
+    # X_eis = add_key_from_dict_to_dict(Eiscat_support, X_eis, key="r_h")
+    # X_eis = add_key_with_matching_times(Eiscat_support, X_eis, key="r_error")
     
     
-    print(inspect_dict(X_eis))
+    # # print(inspect_dict(X_eis))
     
-    X_kian = add_key_from_dict_to_dict(Eiscat_support, X_kian, key="r_h")
-    X_art = add_key_from_dict_to_dict(Eiscat_support, X_art, key="r_h")
-    X_ion = convert_ionograms_to_dict(ion, X_eis)
+    # X_kian = add_key_from_dict_to_dict(Eiscat_support, X_kian, key="r_h")
+    # X_art = add_key_from_dict_to_dict(Eiscat_support, X_art, key="r_h")
+    # X_ion = convert_ionograms_to_dict(ion, X_eis)
     
 
 
-    save_dict(X_eis, "X_eis.pkl")
-    save_dict(X_kian, "X_kian.pkl")
-    save_dict(X_art, "X_art.pkl")
-    save_dict(X_ion, "X_ion.pkl")
+    # save_dict(X_eis, "X_eis.pkl")
+    # save_dict(X_kian, "X_kian.pkl")
+    # save_dict(X_art, "X_art.pkl")
+    # save_dict(X_ion, "X_ion.pkl")
 
 
 
