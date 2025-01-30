6c1827751f24ed3222065d9e32be24fd0a92a76d
Kian0902 <kian0902@hotmail.com>
2025-01-30 13:21:08 +0100

    Made class for storing input and target data.

--- Eiscat/Processing/data_denoise.py
+++ Eiscat/Processing/data_denoise.py
@@ -6,12 +6,27 @@ Created on Thu Jan 30 13:12:37 2025
 """
 
 
-
-
+import os
 import numpy as np
+import matplotlib.pyplot as plt
+
+
+import torch
+from torch.utils.data import Dataset
 
 
 
+class StoreDataset(Dataset):
+    def __init__(self, data, targets):
+        self.targets = torch.tensor(np.array(targets), dtype=torch.float32)
+        self.targets = torch.tensor(np.array(targets), dtype=torch.float32)
+        
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, idx):
+        
+        return self.data[idx], self.targets[idx]
 
 
 
