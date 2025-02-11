c16c67bf3bccc271526dbe37a2dd843932bdf6c4
Kian0902 <kian0902@hotmail.com>
2025-02-11 13:33:06 +0100

    Updated

--- Ionogram/main_ionogram.py
+++ Ionogram/main_ionogram.py
@@ -11,16 +11,19 @@ import os
 import numpy as np
 import matplotlib.pyplot as plt
 from ionogram_sorting import IonogramSorting
-# from process_ionograms import IonogramProcessing
+from iono_utils import save_dict, load_dict, inspect_dict
+
+
+from process_ionograms import IonogramProcessing
 from tqdm import tqdm
 
 
 
+# X = load_dict("sorted_ionogram_dicts/2019-1-1.pkl")
 
 
 
-
-datapath_folder = "Ionogram_TXT"
+datapath_folder = "Iono_TXT"
 
 
 for file in tqdm(os.listdir(datapath_folder)):
@@ -32,10 +35,12 @@ for file in tqdm(os.listdir(datapath_folder)):
     A = IonogramSorting()
     print("-Sorting Mothly file into 15 min ionogram samples...")
     A.import_data(file_path)
+    A.save_as_dict(folder_path="sorted_ionogram_dicts")
     # A.save_dataset()
     # data = A.return_dataset()
     # times, data = A.import_data(file_path)
     print("-Sorting Complete!\n")
+    # break
     
     # B = IonogramProcessing()
     # print("-Making Ionogram images...")
