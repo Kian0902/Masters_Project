2cacfbb86c67e49900c49f96cee3967069766a11
Kian0902 <kian0902@hotmail.com>
2025-02-11 13:23:04 +0100

    Made utils file for ionogramming

---
+++ Ionogram/iono_utils.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb 11 10:59:31 2025
+
+@author: kian0
+"""
+
+
+
+import pickle 
+
+def load_dict(file_name):
+    with open(file_name, 'rb') as f:
+        dataset = pickle.load(f)
+    return dataset
+
+def save_dict(dataset: dict, file_name: str):
+    with open(file_name, 'wb') as file:
+        pickle.dump(dataset, file)
+
+def inspect_dict(d, indent=0):
+    """
+    Recursively print all keys in a nested dictionary with the shape of their values.
+    
+    :param d: The dictionary to process.
+    :param indent: The current indentation level (used for nested items).
+    """
+    for key, value in d.items():
+        # Create indentation based on the depth level
+        prefix = '  ' * indent
+        
+        # Determine the type and shape of the value
+        if isinstance(value, dict):
+            value_shape = f"dict with {len(value)} keys"
+        elif isinstance(value, list):
+            value_shape = f"list of length {len(value)}"
+        elif isinstance(value, set):
+            value_shape = f"set of length {len(value)}"
+        elif isinstance(value, tuple):
+            value_shape = f"tuple of length {len(value)}"
+        elif isinstance(value, np.ndarray):
+            value_shape = f"numpy array with shape {value.shape}"
+        else:
+            value_shape = f"type {type(value).__name__}"
+        
+        # Print the key along with its value shape
+        print(f"{prefix}{key}: ({value_shape})")
+        
+        # If the value is also a dictionary, recursively call the function
+        if isinstance(value, dict):
+            inspect_dict(value, indent=indent + 1)
