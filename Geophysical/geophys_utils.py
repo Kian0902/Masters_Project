06afa10ef74637e75053978285aca1f92e6d7c45
Kian0902 <kian0902@hotmail.com>
2025-04-06 17:48:48 +0200

    Added support functions

--- Geophysical/geophys_utils.py
+++ Geophysical/geophys_utils.py
@@ -7,8 +7,10 @@ Created on Sun Dec  1 15:53:17 2024
 
 
 import os
+import pandas as pd
 from datetime import datetime
 
+from tqdm import tqdm
 
 
 def filename_to_datetime(folder_name):
@@ -30,6 +32,46 @@ def filename_to_datetime(folder_name):
             datetimes.append(dt)
     
     return datetimes
+
+
+
+
+
+def create_samples_csv(dataset, output_dir="geophys_samples"):
+    
+    
+    os.makedirs(output_dir, exist_ok=True)
+    
+    for i, row in tqdm(dataset.iterrows()):
+        filename = row['DateTime'].strftime('%Y%m%d_%H%M') + '.csv'
+        file_path = os.path.join(output_dir, filename)
+        
+        sample = row.drop(labels=['DateTime']).to_frame().transpose()
+        sample.to_csv(file_path, index=False, header=False)
+        # break
+        
+    print(f"CSV files saved in '{output_dir}'")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
